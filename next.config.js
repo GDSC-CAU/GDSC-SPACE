81f0236c84290c7e5e33178e9acb43e4c5a365c0
Danpa_cho <danpa725@cau.ac.kr>
2023-07-04 21:31:36 +0900

    setup: next.config for production

---
+++ next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    swcMinify: true,
+    reactStrictMode: true,
+    pageExtensions: ['md', 'mdx', 'tsx', 'ts'],
+    compiler:
+        process.env.NODE_ENV === 'production'
+            ? {
+                  removeConsole: {
+                      exclude: ['error'],
+                  },
+              }
+            : {},
+    experimental: {
+        // Enable Google Analytics
+        nextScriptWorkers: true,
+    },
+}
+
+module.exports = nextConfig
