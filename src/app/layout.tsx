1356e3a1e72a27a86204326d11264346653442de
yymin1022 <yymin1022@gmail.com>
2023-07-25 20:30:50 +0900

    feat: Moved Link from page to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,10 +8,21 @@ export const metadata = {
     description: 'welcome to space',
 }
 
+const Links = () => {
+    return (
+        <>
+            <p>Links</p>
+        </>
+    )
+}
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="kr">
-            <body className={`${inter.className} h-screen max-h-screen min-h-screen`}>{children}</body>
+            <body className={`${inter.className} h-screen max-h-screen min-h-screen`}>
+                <Links />
+                <>{children}</>
+            </body>
         </html>
     )
 }
