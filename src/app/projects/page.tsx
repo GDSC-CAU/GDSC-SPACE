7db6e8542a29c8ac5799f80ae5a4c1400837c5b9
yymin1022 <yymin1022@gmail.com>
2023-07-25 20:33:52 +0900

    feat: Added ProjectList Component

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,7 @@
+export default function ProjectList() {
+    return (
+        <main className="flex h-full w-full flex-col items-center justify-between p-24">
+            <p>This is Project List</p>
+        </main>
+    )
+}
