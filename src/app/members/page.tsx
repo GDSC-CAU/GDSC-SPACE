9b00a5909ea2f4d9eb01e7ebb7b5c80231a7aa9c
yymin1022 <yymin1022@gmail.com>
2023-07-25 20:33:22 +0900

    feat: Added MemberList Component

---
+++ src/app/members/page.tsx
@@ -0,0 +1,7 @@
+export default function MemberList() {
+    return (
+        <main className="flex h-full w-full flex-col items-center justify-between p-24">
+            <p>This is Member List</p>
+        </main>
+    )
+}
