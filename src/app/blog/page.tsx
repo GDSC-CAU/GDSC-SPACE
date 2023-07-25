7e1a3400e31ceab802f7aaedaad44ae77b2e0fcd
yymin1022 <yymin1022@gmail.com>
2023-07-25 20:32:41 +0900

    feat: Added BlogList Component

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,7 @@
+export default function BlogList() {
+    return (
+        <main className="flex h-full w-full flex-col items-center justify-between p-24">
+            <p>This is Blog List</p>
+        </main>
+    )
+}
