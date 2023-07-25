35cc077e1de435174736babad0ece0c6125df6ff
yymin1022 <yymin1022@gmail.com>
2023-07-25 20:33:02 +0900

    feat: Added EventList Component

---
+++ src/app/events/page.tsx
@@ -0,0 +1,7 @@
+export default function EventList() {
+    return (
+        <main className="flex h-full w-full flex-col items-center justify-between p-24">
+            <p>This is Event List</p>
+        </main>
+    )
+}
