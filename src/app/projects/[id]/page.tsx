deb3ffce4c8fd12d5309d66fcce4d519549605f1
yymin1022 <yymin1022@gmail.com>
2023-07-25 20:37:59 +0900

    feat: Added ProjectView Component

---
+++ src/app/projects/[id]/page.tsx
@@ -0,0 +1,8 @@
+export default function ProjectView({ params }: { params: { id: string } }) {
+    const projectID = params.id
+    return (
+        <main className="flex h-full w-full flex-col items-center justify-between p-24">
+            <p>This is Project View of {projectID}</p>
+        </main>
+    )
+}
