f6148362d2ee43ec8723181401adeb640a8cedff
Danpa_cho <danpa725@cau.ac.kr>
2023-07-31 20:09:18 +0900

    feat: add common link component with typed link path

---
+++ src/components/link/Link.tsx
@@ -0,0 +1,15 @@
+import Link, { type LinkProps } from 'next/link'
+
+/**
+ * Project link path
+ */
+export type LinkPath = '/' | '/members' | '/blog' | '/events'
+
+interface Link$Props extends Omit<React.AnchorHTMLAttributes<HTMLAnchorElement>, keyof LinkProps>, LinkProps {
+    href: LinkPath
+}
+export const Link$ = ({ href, children, ...linkProps }: React.PropsWithChildren<Link$Props>) => (
+    <Link {...linkProps} href={href}>
+        {children}
+    </Link>
+)
