5cf8cbd0be687e37ca2e1fe3ef57b2f344e84ed3
Danpa_cho <danpa725@cau.ac.kr>
2023-07-31 23:36:28 +0900

    design: change bulk banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
-const Logo = () => {
+const BulkBanner = () => {
     return (
-        <div className="flex flex-col items-center justify-center gap-32">
+        <div className="flex h-72 w-full flex-col items-center justify-center gap-32 rounded-3xl border border-neutral-800 bg-theme-background py-40 text-sm">
             <div className="flex flex-row gap-0">
-                <h1 className="bg-gradient-to-br from-primary-hotpink to-primary-hotpink bg-clip-text text-7xl font-bold text-transparent">
+                <h1 className="bg-gradient-to-br from-primary-hotpink to-primary-hotpink bg-clip-text font-eng text-7xl font-bold text-transparent">
                     G
                 </h1>
-                <h1 className="bg-gradient-to-br from-primary-yellow to-yellow-500 bg-clip-text text-7xl font-bold text-transparent">
+                <h1 className="bg-gradient-to-br from-primary-yellow to-yellow-500 bg-clip-text font-eng text-7xl font-bold text-transparent">
                     D
                 </h1>
-                <h1 className="bg-gradient-to-br from-primary-blue to-sky-500 bg-clip-text text-7xl font-bold text-transparent">
+                <h1 className="bg-gradient-to-br from-primary-blue to-sky-500 bg-clip-text font-eng text-7xl font-bold text-transparent">
                     S
                 </h1>
-                <h1 className="bg-gradient-to-br from-primary-teal to-green-500 bg-clip-text text-7xl font-bold text-transparent">
+                <h1 className="bg-gradient-to-br from-primary-teal to-green-500 bg-clip-text font-eng text-7xl font-bold text-transparent">
                     C
                 </h1>
             </div>
@@ -22,12 +22,8 @@ const Logo = () => {
 
 export default function Home() {
     return (
-        <>
-            <div className="flex h-full w-full flex-col items-center justify-between">
-                <div className="py-40">
-                    <Logo />
-                </div>
-            </div>
-        </>
+        <div className="h-full w-full">
+            <BulkBanner />
+        </div>
     )
 }
