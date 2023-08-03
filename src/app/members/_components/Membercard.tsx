1940ee2943c6736303c3ec9372b2a2cb84946670
jujured <pjiww4@gmail.com>
2023-08-03 20:51:00 +0900

    feat: test

---
+++ src/app/members/_components/Membercard.tsx
@@ -0,0 +1,9 @@
+const MemberCard = () => {
+    return (
+        <div className="m-4 flex h-64 w-64 flex-col items-center justify-center overflow-hidden rounded-lg bg-white shadow-lg">
+            <h1 className="text-xl font-medium text-gray-900">ㅎㅎ</h1>
+        </div>
+    )
+}
+
+export default MemberCard
--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,7 +1,3 @@
 export default function MemberList() {
-    return (
-        <main className="flex h-full w-full flex-col items-center justify-between p-24">
-            <p>This is Member List</p>
-        </main>
-    )
+    return <main className="flex h-full w-full flex-col items-center justify-between p-24"></main>
 }
