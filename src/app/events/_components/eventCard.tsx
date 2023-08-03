1f98f8924b140b040748b9b3a2b1282530c1ebeb
yeojin <yeojin7010+L@gmail.com>
2023-08-03 21:28:09 +0900

    feat: create css fram

---
+++ src/app/events/_components/eventCard.tsx
@@ -0,0 +1,12 @@
+// import Link from 'next/link'
+
+function EventCard() {
+    return (
+        <div className="h-32 w-32 rounded-xl">
+            <div className="h-6 w-full rounded-t-xl bg-orange-400"></div>
+            <div className="h-24 w-full rounded-b-xl bg-black"></div>
+        </div>
+    )
+}
+
+export default EventCard
