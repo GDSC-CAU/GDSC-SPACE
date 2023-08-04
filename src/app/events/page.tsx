1eb26ab6eb7543189451f3bc8359b073ffe26c76
yeojin <yeojin7010+L@gmail.com>
2023-08-04 23:21:19 +0900

    feat: add <Link> to EventCard component

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import EventCard from './_components/eventCard'
+import { EventCard } from './_components/eventCard'
 
 export default function EventList() {
     return (
@@ -7,9 +7,7 @@ export default function EventList() {
             <p>This is Event List</p>
             <Link href="/events/1">Event 1</Link>
             <Link href="/events/2">Event 2</Link>
-            <Link href="/events/3">
-                <EventCard />
-            </Link>
+            <EventCard id={'3'} />
         </main>
     )
 }
