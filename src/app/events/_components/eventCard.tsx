0a118cf5f3689583ea2984848d1cfb5641ee8339
yeojin <yeojin7010+L@gmail.com>
2023-08-12 23:01:07 +0900

    feat: event card props 수정

--- src/app/events/_components/eventCard.tsx
+++ src/app/events/_components/eventCard.tsx
@@ -1,33 +1,25 @@
 import Image from 'next/image'
 import Link from 'next/link'
-
-type eventcard = {
-    id: string
-    category: string
-    date: string
-    title: string
-    content: string
-    image: string
-}
+import { eventData } from '../page'
 
 const imageStyle = {
     borderRadius: '5% 5% 0% 0%',
 }
 
-const EventCard = ({ id, category, date, title, content, image }: eventcard) => {
+const EventCard = ({ event }: { event: eventData }) => {
     return (
-        <Link href={`/events/${id}`}>
+        <Link href={`/events/${event.id}`}>
             <div className="group relative h-80 w-60">
                 <div className="relative flex h-1/2 w-full items-center justify-end rounded-t-xl">
-                    <Image fill src={image} alt="picture" style={imageStyle}></Image>
+                    <Image fill src={event.image} alt="picture" style={imageStyle}></Image>
                 </div>
                 <div className="absolute h-1/2 w-full rounded-b-xl bg-black ">
                     <div className="px-4 pb-2 pt-4 font-kor text-xs">
                         {' '}
-                        {category} ﹒ {date}
+                        {event.category} ﹒ {event.date}
                     </div>
-                    <div className="px-4 font-eng text-xl"> {title} </div>
-                    <div className="p-4 font-kor text-sm"> {content} </div>
+                    <div className="px-4 font-eng text-xl"> {event.title} </div>
+                    <div className="p-4 font-kor text-sm"> {event.content} </div>
                 </div>
             </div>
         </Link>
