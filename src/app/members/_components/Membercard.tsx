78fde5a69516fb29a9ec5ff27ddb713be2d4d8ef
jujured <pjiww4@gmail.com>
2023-08-14 17:08:19 +0900

    feat: MemberCard

--- src/app/members/_components/Membercard.tsx
+++ src/app/members/_components/Membercard.tsx
@@ -1,9 +1,36 @@
-const MemberCard = () => {
+import Image from 'next/image'
+import Link from 'next/link'
+import { memberData } from '../page'
+
+const imageStyle = {
+    borderRadius: '5% 5% 5% 5%',
+}
+
+const infoStyle = {
+    borderRadius: '50% 0% 5% 5%',
+    height: '110px',
+}
+
+const MemberCard = ({ member }: { member: memberData }) => {
     return (
-        <div className="m-4 flex h-64 w-64 flex-col items-center justify-center overflow-hidden rounded-lg bg-white shadow-lg">
-            <h1 className="text-xl font-medium text-gray-900">ㅎㅎ</h1>
-        </div>
+        <Link href={`/members/${member.id}`}>
+            <div className="h-200 group relative w-40">
+                <div className="flex h-[250px] w-full items-center justify-start rounded-t-xl">
+                    <Image src={member.image} alt="mem_image" layout="fill" objectFit="cover" style={imageStyle} />
+                </div>
+                <div
+                    className="absolute bottom-0 left-0 w-full rounded-b-xl bg-white p-2"
+                    style={{ ...infoStyle, boxShadow: '-1px -2px 6px rgba(0, 0, 0, 0.2)' }}
+                >
+                    <div className="mt-1 flex justify-end px-1">
+                        <Image src="/GDSC_logo.png" alt="GDSC_logo" width={40} height={40} />
+                    </div>
+                    <div className="mt-5 px-1 text-right font-kor text-lg text-black">{member.name}</div>
+                    <div className="px-1 text-right font-eng text-xs text-black">{member.role}</div>
+                </div>
+            </div>
+        </Link>
     )
 }
 
-export default MemberCard
+export { MemberCard }
--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,3 +1,25 @@
+//import Link from 'next/link'
+import { MemberCard } from './_components/Membercard'
+
+export type memberData = {
+    id: string
+    name: string
+    role: string
+    image: string
+}
+
 export default function MemberList() {
-    return <main className="flex h-full w-full flex-col items-center justify-between p-24"></main>
+    const members: memberData = {
+        id: '1',
+        name: '유용민',
+        role: 'Leader/Developer',
+        image: '/yymin1022.png',
+    }
+
+    return (
+        <main className="flex h-full w-full flex-col items-center justify-between p-24">
+            <p>Member List</p>
+            <MemberCard member={members} />
+        </main>
+    )
 }
