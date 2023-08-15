ad4b13800b386e4957d7fbbe4ff25766e3514682
jujured <pjiww4@gmail.com>
2023-08-15 14:40:22 +0900

    feat:멤버카드 세부정보 추가

--- src/app/members/_components/Membercard.tsx
+++ src/app/members/_components/Membercard.tsx
@@ -6,14 +6,6 @@ import { useState } from 'react'
 import { Gdsc } from '~/components/icons'
 import { Member } from '../page'
 
-const imageStyle = {
-    borderRadius: '5% 5% 5% 5%',
-}
-
-const infoStyle = {
-    borderRadius: '50% 0% 5% 5%',
-}
-
 const MemberCard = ({ member }: { member: Member }) => {
     const [showIntroduction, setShowIntroduction] = useState(false)
 
@@ -23,24 +15,30 @@ const MemberCard = ({ member }: { member: Member }) => {
 
     const getImgSrc = () => {
         if (!member.imagesrc) {
-            return member.gender === 'Female' ? '/GDSC_logo.png' : '/test_image.png'
+            return member.gender === 'Female' ? '/female.png' : '/male.png'
         }
         return member.imagesrc
     }
 
     return (
         <div
-            className={`perspective group relative h-[250px] w-40 cursor-pointer transition-all hover:scale-105`}
+            className="group relative h-[250px] w-40 cursor-pointer transition-transform transform-gpu hover:scale-105"
             onClick={toggleIntroduction}
         >
-            <div className="preserve-3d group-hover:my-rotate-y-180 relative h-full w-full duration-1000">
+            <div className="preserve-3d group-hover:my-rotate-y-180 duration-5000 relative h-full w-full">
                 {/* Front Content */}
                 <div className="absolute h-full w-full backface-hidden">
                     <Link href={`/members/${member.name}`}>
-                        <Image fill src={getImgSrc()} alt="mem_image" objectFit="cover" style={imageStyle} />
+                        <Image
+                            fill
+                            src={getImgSrc()}
+                            alt="mem_image"
+                            objectFit="cover"
+                            style={{ borderRadius: '5% 5% 5% 5%' }}
+                        />
                         <div
                             className="absolute bottom-0 left-0 h-[110px] w-full rounded-b-xl bg-white p-2"
-                            style={{ ...infoStyle, boxShadow: '-1px -2px 6px rgba(0, 0, 0, 0.3)' }}
+                            style={{ borderRadius: '50% 0% 5% 5%', boxShadow: '-1px -2px 6px rgba(0, 0, 0, 0.3)' }}
                         >
                             <div className="flex justify-end px-1">
                                 <Gdsc width={40} height={40} />
@@ -57,14 +55,19 @@ const MemberCard = ({ member }: { member: Member }) => {
                         showIntroduction ? '' : 'hidden'
                     }`}
                 >
-                    <div className="group relative h-[250px] w-40 bg-white" style={{ ...imageStyle }}>
-                        <div className="margin" style={{ padding: '10px' }}>
+                    <div className="group relative h-[250px] w-40 bg-white" style={{ borderRadius: '5% 5% 5% 5%' }}>
+                        <div className="mx-2" style={{ padding: '10px' }}>
                             <div className="text-left font-kor text-lg text-black" style={{ fontWeight: 500 }}>
                                 {member.nickname}
                             </div>
                             <div className="text-left font-eng text-xs text-primary-purple">{member.position}</div>
                             <hr className="my-2 border-primary-whitegray" />
                             <div className="text-left font-kor text-xs text-black">{member.introduction}</div>
+                            <div className="absolute bottom-0 my-2">
+                                <div className="text-left font-kor text-xs text-black">{member.github}</div>
+                                <div className="text-left font-kor text-xs text-black">{member.email}</div>
+                                <div className="text-left font-kor text-xs text-black">{member.instagram}</div>
+                            </div>
                         </div>
                     </div>
                 </div>
--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -9,6 +9,9 @@ export type Member = {
     year: '1기' | '2기' | '3기'
     position: string
     introduction: string
+    github?: string
+    email?: string
+    instagram?: string
 }
 
 export default function MemberList() {
@@ -22,6 +25,9 @@ export default function MemberList() {
             year: '2기',
             position: 'Cloud Server / flutter',
             introduction: '안녕하세요, GDSC 3기 리더 유용민입니다. 잘 부탁드립니다:)',
+            github: 'github.com/yymin1022',
+            email: 'yymin1022@gmail.com',
+            instagram: '@useful_min',
         },
         {
             name: '장준성',
@@ -31,6 +37,8 @@ export default function MemberList() {
             year: '2기',
             position: 'Front-End',
             introduction: '안녕하세요~!~!',
+            github: 'github.com/danpacho',
+            email: 'danpa725@cau.ac.kr',
         },
         {
             name: '박지우',
@@ -39,6 +47,8 @@ export default function MemberList() {
             year: '3기',
             position: 'Front-End',
             introduction: '안녕하세용!!',
+            github: 'github.com/jujuredt',
+            email: 'juju.0_0._',
         },
     ]
 
