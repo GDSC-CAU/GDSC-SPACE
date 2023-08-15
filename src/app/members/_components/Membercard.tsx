4163a95a56f3c5c92399752be6aa2b85e91a325c
jujured <pjiww4@gmail.com>
2023-08-15 13:57:00 +0900

    feat: membercard detail

--- src/app/members/_components/Membercard.tsx
+++ src/app/members/_components/Membercard.tsx
@@ -1,8 +1,10 @@
+'use client'
+
 import Image from 'next/image'
 import Link from 'next/link'
 import { useState } from 'react'
 import { Gdsc } from '~/components/icons'
-import { MemberData } from '../page'
+import { Member } from '../page'
 
 const imageStyle = {
     borderRadius: '5% 5% 5% 5%',
@@ -12,30 +14,30 @@ const infoStyle = {
     borderRadius: '50% 0% 5% 5%',
 }
 
-const MemberCard = ({ member }: { member: MemberData }) => {
+const MemberCard = ({ member }: { member: Member }) => {
     const [showIntroduction, setShowIntroduction] = useState(false)
-    const [isHovered, setIsHovered] = useState(false)
 
     const toggleIntroduction = () => {
-        setShowIntroduction(!showIntroduction)
+        setShowIntroduction((showIntroduction) => !showIntroduction)
+    } //이전 상태 조회할 때, 함수 넣기
+
+    const getImgSrc = () => {
+        if (!member.imagesrc) {
+            return member.gender === 'Female' ? '/GDSC_logo.png' : '/test_image.png'
+        }
+        return member.imagesrc
     }
 
     return (
         <div
-            className={`perspective group relative h-[250px] w-40 cursor-pointer`}
+            className={`perspective group relative h-[250px] w-40 cursor-pointer transition-all hover:scale-105`}
             onClick={toggleIntroduction}
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
-            style={{
-                transform: isHovered ? 'scale(1.05)' : 'scale(1)',
-                transition: 'transform 0.3s ease-in-out',
-            }}
         >
             <div className="preserve-3d group-hover:my-rotate-y-180 relative h-full w-full duration-1000">
                 {/* Front Content */}
-                <div className="backface-hidden absolute h-full w-full">
+                <div className="absolute h-full w-full backface-hidden">
                     <Link href={`/members/${member.name}`}>
-                        <Image fill src={member.image} alt="mem_image" objectFit="cover" style={imageStyle} />
+                        <Image fill src={getImgSrc()} alt="mem_image" objectFit="cover" style={imageStyle} />
                         <div
                             className="absolute bottom-0 left-0 h-[110px] w-full rounded-b-xl bg-white p-2"
                             style={{ ...infoStyle, boxShadow: '-1px -2px 6px rgba(0, 0, 0, 0.3)' }}
@@ -51,7 +53,7 @@ const MemberCard = ({ member }: { member: MemberData }) => {
 
                 {/* Back Content */}
                 <div
-                    className={`my-rotate-y-180 backface-hidden absolute h-full w-full overflow-hidden ${
+                    className={`my-rotate-y-180 absolute h-full w-full overflow-hidden backface-hidden ${
                         showIntroduction ? '' : 'hidden'
                     }`}
                 >
--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,35 +1,52 @@
-'use client'
-import { useEffect, useState } from 'react'
 import { MemberCard } from './_components/Membercard'
 
-export type MemberData = {
+export type Member = {
     name: string
+    gender: 'Male' | 'Female'
     nickname?: string
-    role: 'lead' | 'core' | 'member'
-    image: string
+    role: 'Lead' | 'Core' | 'Member'
+    imagesrc?: string
+    year: '1기' | '2기' | '3기'
     position: string
     introduction: string
 }
 
 export default function MemberList() {
-    const [isClient, setIsClient] = useState(false)
-
-    useEffect(() => {
-        setIsClient(true)
-    }, [])
-
-    const members: MemberData = {
-        name: '유용민',
-        nickname: 'YYMIN',
-        role: 'lead',
-        image: '/GDSC_logo.png',
-        position: 'Cloud Server / flutter',
-        introduction: '안녕하세요, GDSC 3기 리더 유용민입니다. 잘 부탁드립니다:)',
-    }
+    const members: Member[] = [
+        {
+            name: '유용민',
+            gender: 'Male',
+            nickname: 'DEV.LR',
+            role: 'Lead',
+            imagesrc: '/GDSC_logo.png',
+            year: '2기',
+            position: 'Cloud Server / flutter',
+            introduction: '안녕하세요, GDSC 3기 리더 유용민입니다. 잘 부탁드립니다:)',
+        },
+        {
+            name: '장준성',
+            gender: 'Male',
+            nickname: '단팥초',
+            role: 'Core',
+            year: '2기',
+            position: 'Front-End',
+            introduction: '안녕하세요~!~!',
+        },
+        {
+            name: '박지우',
+            gender: 'Female',
+            role: 'Member',
+            year: '3기',
+            position: 'Front-End',
+            introduction: '안녕하세용!!',
+        },
+    ]
 
     return (
-        <main className="flex h-full w-full flex-col items-center justify-between p-24">
-            {isClient && <MemberCard member={members} />}
+        <main className="flex-raw flex h-full w-full items-center justify-between p-24">
+            {members.map((member) => (
+                <MemberCard member={member} key={`${member.name}-${member.position}`} />
+            ))}
         </main>
     )
 }
