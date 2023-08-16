8165de30f086204689d15a734fd7f2a722abe2fa
sonyujin <jainayj804998@gmail.com>
2023-08-16 19:40:20 +0900

    feat: Event page detail2

--- src/app/events/[id]/page.tsx
+++ src/app/events/[id]/page.tsx
@@ -51,12 +51,7 @@ export default function EventView({ params }: { params: { id: string } }) {
                 </div>
 
                 <div className="text-center">▴ GDSC 잡 페어 행사</div>
-                <p className="mb-28 mt-12">
-                    Google Developer Student Clubs (GDSC) 프로그램은 학생들이 개발/리더십 능력을 키울 수 있도록 지원하는
-                    프로그램입니다. 기술적 성장을 위한 교육자료를 제공하고 리더십 개발을 위한 다양한 활동들을
-                    지원합니다. 학생이 성장할 수 있도록 전세계의 학생 Lead들과의 교류 기회, Google의 이벤트 참여, 현업
-                    엔지니어와의 만남 등 다양한 기회를 제공하고 쇼케이스/워크샵을 통해 역량을 높일 수 있도록 돕습니다.
-                </p>
+                <p className="mb-28 mt-12"></p>
                 <div className="flex w-full items-center justify-center bg-black">
                     <Image
                         src="https://cdn.pixabay.com/photo/2023/07/19/04/56/european-shorthair-8136065_1280.jpg"
