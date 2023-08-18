f7de03f84af5b3b6bce4255c74415f988b385b4b
yeojin <yeojin7010+L@gmail.com>
2023-08-18 22:10:00 +0900

    feat: event card zoom in interaction

--- src/app/events/_components/eventCard.tsx
+++ src/app/events/_components/eventCard.tsx
@@ -2,16 +2,18 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { Event } from '../page'
 
-const imageStyle = {
-    borderRadius: '5% 5% 0% 0%',
-}
-
 const EventCard = ({ event }: { event: Event }) => {
     return (
         <Link href={`/events/${event.id}`}>
             <div className="group relative h-80 w-60">
-                <div className="relative flex h-1/2 w-full items-center justify-end rounded-t-xl">
-                    <Image fill src={event.image} alt="picture" style={imageStyle}></Image>
+                <div className="relative flex h-1/2 w-full items-center justify-end overflow-hidden rounded-t-xl">
+                    <Image
+                        fill
+                        src={event.image}
+                        alt="picture"
+                        objectFit={'cover'}
+                        className="h-full w-full rounded-t-lg duration-500 ease-in-out group-hover:scale-110"
+                    ></Image>
                 </div>
                 <div className="absolute h-1/2 w-full rounded-b-xl bg-black ">
                     <div className="px-4 pb-2 pt-4 font-kor text-xs">
