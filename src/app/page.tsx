5c60e579327e927cf517be7329aa87c4138a022e
sonyujin <jainayj804998@gmail.com>
2023-08-20 02:02:13 +0900

    feat: transform and hover

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { type } from 'os'
 import ProjectCard from './projects/_components/ProjectCard'
 const BulkBanner = () => {
     return (
@@ -22,14 +23,32 @@ const BulkBanner = () => {
 }
 
 export default function Home() {
-    const p1 = '/d.png'
-    const p2 = '/f.png'
     return (
         <div className="h-full w-full">
             <BulkBanner />
-            <div className="flex flex-col items-center justify-center gap-5 p-5">
-                <ProjectCard picture={p1} name="Project Wiro" d1="작업 기간 / 개발자" d2="(두줄 소개)" bool={true} />
-                <ProjectCard picture={p2} name="Carbon Hero" d1=":Save The Earth" d2="asfdghjfhdgsf" bool={true} />
+            <div className="mt-28 flex flex-col items-center justify-center gap-6">
+                <div className="bg-gradient-to-b from-blue-700 to-white bg-clip-text text-5xl font-extrabold text-transparent">
+                    Project
+                </div>
+                <div className="mb-12">우리는 이러한 문제점들을 읽어내고, 해결책을 탐구합니다!</div>
+            </div>
+            <div className="flex flex-col items-center justify-center gap-10 ">
+                {/* <ProjectCard name="Wiro" d1="작업기간 / 개발자" d2="(두줄 소개)" isReverse={true} />
+                <ProjectCard name="Carbon Hero" d1=":Save The Earth" d2="asfdghjfhdgsf" isReverse={false} />
+                <ProjectCard name="Wiro" d1="작업기간 / 개발자" d2="(두줄 소개)" isReverse={true} />
+                <ProjectCard name="Carbon Hero" d1=":Save The Earth" d2="asfdghjfhdgsf" isReverse={false} />
+             */}
+                {API_MAIN_PROJECTS.MAIN_PROJECT_LIST.map((project, index) => (
+                    <ProjectCard
+                        key={index}
+                        title={project.PROJECT_TITLE}
+                        image={project.PROJECT_IMAGE}
+                        image_sub={project.PROJECT_IMAGE_SUB}
+                        description={project.PROJECT_DESCRIPTION}
+                        subtitle={project.PROJECT_SUBTITLE}
+                        isReverse={index % 2 === 0} // 홀수 인덱스일 때는 true, 짝수 인덱스일 때는 false
+                    />
+                ))}
             </div>
         </div>
     )
--- src/app/projects/_components/ProjectCard.tsx
+++ src/app/projects/_components/ProjectCard.tsx
@@ -1,286 +1,164 @@
-//import Image from 'next/image'
-import Image from 'next/legacy/image'
+'use client'
+import Image from 'next/image'
 import Link from 'next/link'
+import React, { useEffect, useRef, useState } from 'react'
+import { API_MAIN_PROJECTS } from 'src/app/page'
+import { MAIN_PROJECT_DATA } from 'src/app/page'
 
-export default function ProjectCard(props: any) {
+export type API_MAIN_PROJECTS = {
+    MAIN_PROJECT_CNT: number
+    MAIN_PROJECT_LIST: Array<MAIN_PROJECT_DATA>
+}
+
+export type MAIN_PROJECT_DATA = {
+    PROJECT_DESCRIPTION: string
+    PROJECT_IMAGE: string
+    PROJECT_IMAGE_SUB: string
+    PROJECT_SUBTITLE: string
+    PROJECT_TITLE: string
+}
+const API_MAIN_PROJECTS: API_MAIN_PROJECTS = {
+    MAIN_PROJECT_CNT: 3,
+    MAIN_PROJECT_LIST: [
+        {
+            PROJECT_DESCRIPTION: '(두줄소개)',
+            PROJECT_IMAGE: 'https://cdn.pixabay.com/photo/2023/08/11/18/35/flowers-8184126_1280.jpg',
+            PROJECT_IMAGE_SUB: 'https://cdn.pixabay.com/photo/2023/08/05/15/15/waves-8171279_1280.jpg',
+            PROJECT_SUBTITLE: '작업기간 / 개발자',
+            PROJECT_TITLE: 'Wiro',
+        },
+        {
+            PROJECT_DESCRIPTION:
+                "This is a Solution for the people who don't know what actions could be taken toreduce green house effect.This is an education game. By playing this game, the players are informedwhat could be done in real world to decrease the green house effect andwhat contributes to the global warming and green house effect.",
+            PROJECT_IMAGE: 'https://cdn.pixabay.com/photo/2023/08/11/18/35/flowers-8184126_1280.jpg',
+            PROJECT_IMAGE_SUB: 'https://cdn.pixabay.com/photo/2023/08/05/15/15/waves-8171279_1280.jpg',
+            PROJECT_SUBTITLE: ': Save The Earth',
+            PROJECT_TITLE: 'Carbon Hero',
+        },
+        {
+            PROJECT_DESCRIPTION:
+                "Our goal is to help blind people recognize menus and order without assistance. The most important thing was to capture the menu through the user's camera and present the menu in the most convenient way for blind people: by sound and optimized UI.",
+            PROJECT_IMAGE: 'https://cdn.pixabay.com/photo/2023/08/11/18/35/flowers-8184126_1280.jpg',
+            PROJECT_IMAGE_SUB: 'https://cdn.pixabay.com/photo/2023/08/05/15/15/waves-8171279_1280.jpg',
+            PROJECT_SUBTITLE: 'to blind people can easily order',
+            PROJECT_TITLE: 'Blinder',
+        },
+        {
+            PROJECT_DESCRIPTION:
+                '돌아보면 주변에 참 고마운 분들이 많은 것 같습니다. 이번 기회에 주위의 감사한 사람들에게 하나뿐인 클립 영상으로 마음을 전달해보는 건 어떨까요? 짧은 감사편지를 작성하고, 받는 분에게 어울리는 키워드를 뽑아보세요. 여러분의 감사한 마음을 소중히 담아 영상으로 보내드립니다.',
+            PROJECT_IMAGE: 'https://cdn.pixabay.com/photo/2023/08/11/18/35/flowers-8184126_1280.jpg',
+            PROJECT_IMAGE_SUB: 'https://cdn.pixabay.com/photo/2023/08/05/15/15/waves-8171279_1280.jpg',
+            PROJECT_SUBTITLE: 'Say Thank You',
+            PROJECT_TITLE: 'Thanks Clip',
+        },
+    ],
+}
+interface ProjectCardProps extends API_MAIN_PROJECTS.MAIN_PROJECT_DATA {
+    isReverse: boolean
+}
+
+export default function ProjectCard(props: Omit<API_MAIN_PROJECTS.MAIN_PROJECT_DATA, 'key'>) {
+    const { title, image, image_sub, description, subtitle, isReverse } = props
+    const textDirection = isReverse ? 'items-end' : 'items-start'
+    const LScrollDirection = isReverse ? '-translate-x-full' : '-translate-x-0'
+    const RScrollDirection = isReverse ? '-translate-x-0' : '-translate-x-full'
+
+    const [isVisible, setIsVisible] = useState(false)
+    const ImageRef = useRef(null)
+
+    useEffect(() => {
+        const options = {
+            root: null,
+            rootMargin: '0px',
+            threshold: 0.5,
+        }
+
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    setIsVisible(true)
+                    observer.unobserve(entry.target)
+                }
+            })
+        }, options)
+
+        if (ImageRef.current) {
+            observer.observe(ImageRef.current)
+        }
+
+        return () => {
+            if (ImageRef.current) {
+                observer.unobserve(ImageRef.current)
+            }
+        }
+    }, [])
+
     return (
-        <main>
-            <div className="flex-center flex w-full flex-row items-center justify-center p-6">
-                <div className="w-1/2 ">
-                    <Image
-                        src={props.picture}
-                        alt="윈도우 사진"
-                        priority={true}
-                        style={{
-                            //    width:"100%",
-                            //    height : 'auto',
-                            borderRadius: '15px',
-                        }}
-                        width={663}
-                        height={718}
-                    />
-                </div>
-
-                <div className="relative flex w-1/2 flex-col p-6">
-                    <div className="h-30 flex w-full flex-col justify-items-start">
-                        <h1 className="text-3xl">{props.name}</h1>
-                        <h2 className="text-s">{props.d1}</h2>
-                        <h3 className="text-xs">{props.d2}</h3>
+        <main
+            className={`relative grid w-full  grid-cols-2 items-center justify-between gap-3`}
+            dir={isReverse ? '' : 'rtl'}
+        >
+            <div className="group relative">
+                <Link href="/blog/1">
+                    <div
+                        ref={ImageRef}
+                        className={`relative overflow-hidden transition-all duration-1000 ease-in-out${
+                            isVisible ? 'translate-x-0' : LScrollDirection
+                        }`}
+                    >
+                        <div
+                            className={` flex transition-all duration-1000 ease-in-out ${
+                                isVisible ? 'translate-x-0' : 'translate-x-full'
+                            }`}
+                        >
+                            <Image
+                                src={image}
+                                alt="pixabay picture3"
+                                priority
+                                layout="responsive"
+                                width={663}
+                                height={718}
+                                style={{ borderRadius: '15px' }}
+                                className={`transition duration-300 ease-in-out${
+                                    isVisible ? 'group-hover:brightness-90' : ''
+                                }`}
+                            />
+                        </div>
                     </div>
+                </Link>
+            </div>
+            <div
+                ref={ImageRef}
+                className={`relative overflow-hidden transition-all duration-1000 ease-in-out ${
+                    isVisible ? 'translate-x-0' : RScrollDirection
+                }`}
+            >
+                <div
+                    className={`flex transition-all duration-1000 ease-in-out${
+                        isVisible ? 'translate-x-0' : 'translate-x-full'
+                    }`}
+                >
+                    <div className="group relative grid grid-rows-2 ">
+                        <div className={`max-x-prose flex w-full flex-col items-end ${textDirection}`}>
+                            <h1 className="mb-5 text-4xl font-bold">{title}</h1>
+                            <h2 className="text-xs">{subtitle}</h2>
+                            <h3 className="text-xs">{description}</h3>
+                        </div>
 
-                    <div className="h-20 w-full border bg-white"></div>
+                        <div className={`relative mt-5`}>
+                            <Image
+                                src={image_sub}
+                                alt="pixabay picture3"
+                                priority
+                                layout="responsive"
+                                width={664}
+                                height={180}
+                                style={{ borderRadius: '15px' }}
+                            />
+                        </div>
+                    </div>
                 </div>
             </div>
         </main>
     )
 }
-
-// // import Image from 'next/image'
-// import Link from 'next/link'
-// import Image from 'next/legacy/image';
-// import { relative } from 'path';
-// const imageStyle = {
-//     borderRadius: '10%',
-//     width : '100%',
-//     hiehgt: 'auto'
-// }
-
-// export default function ProjectCard(props: any) {
-//     return (
-//         <main>
-//             {props.bool ? (
-//                 <div className="flex flex-row gap-6 ">
-//                     {/* left : image */}
-//                     <div
-//                         style={{
-//                             height: 310,
-//                             width: 220,
-//                             position: 'relative',
-//                         }}
-//                     >
-//                         <Link href="/projects/1">
-//                             <Image
-//                                 src={props.picture}
-//                                 alt="윈도우 사진"
-//                                 style={imageStyle}
-//                                 layout="fill"
-//                                 priority={true}
-
-//                             />
-//                         </Link>
-//                     </div>
-
-//                     {/* right : name */}
-//                     <div className="flex flex-col">
-//                     <Link
-//                             href="/projects/1"
-//                         >
-//                         <div
-//                             style={{
-//                                 height: 150,
-//                                 width: 180,
-//                                 position: 'relative',
-//                             }}
-//                         >
-//                             <h1>{props.name}</h1>
-//                             <h2>{props.description}</h2>
-//                         </div>
-
-//                             <div
-//                                 className="items-center rounded-2xl border bg-white"
-//                                 style={{
-//                                     height: 160,
-//                                     width: 180,
-//                                     position: 'relative',
-//                                 }}
-//                             >
-//                                 dskflsd
-//                             </div>
-//                         </Link>
-//                         <div></div>
-//                     </div>
-//                 </div>
-//             ) : (
-//                 <div className="flex flex-row-reverse gap-6 ">
-//                 {/* left : image */}
-//                 <div
-//                     style={{
-//                         height: 310,
-//                         width: 220,
-//                         position: 'relative',
-//                     }}
-//                 >
-//                     <Link href="/projects/1">
-//                         <Image
-//                             src={props.picture}
-//                             //width={250}
-//                             //height={400}
-//                             alt="윈도우 사진"
-//                             style={imageStyle}
-//                             layout="fill"
-//                             priority={true}
-//                         />
-//                     </Link>
-//                 </div>
-
-//                 {/* right : name */}
-//                 <div className="flex flex-col">
-//                 <Link
-//                         href="/projects/1"
-//                     >
-//                     <div
-//                         style={{
-//                             height: 150,
-//                             width: 180,
-//                             position: 'relative',
-//                         }}
-//                     >
-//                         <h1>{props.name}</h1>
-//                         <h2>{props.description}</h2>
-//                     </div>
-
-//                         <div
-//                             className="items-center rounded-2xl border bg-white"
-//                             style={{
-//                                 height: 160,
-//                                 width: 180,
-//                                 position: 'relative',
-//                             }}
-//                         >
-//                             dskflsd
-//                         </div>
-//                     </Link>
-//                     <div></div>
-//                 </div>
-//             </div>
-
-//   )}
-//         </main>
-
-//     )
-// }
-
-// // import Image from 'next/image'
-// import Link from 'next/link'
-// import Image from 'next/legacy/image';
-// import { relative } from 'path';
-// const imageStyle = {
-//     borderRadius: '10%',
-//     width : '100%',
-//     hiehgt: 'auto'
-// }
-
-// export default function ProjectCard(props: any) {
-//     return (
-//         <main>
-//             {props.bool ? (
-//                 <div className="flex flex-row gap-6 ">
-//                     {/* left : image */}
-//                     <div
-//                         style={{
-//                             height: 310,
-//                             width: 220,
-//                             position: 'relative',
-//                         }}
-//                     >
-//                         <Link href="/projects/1">
-//                             <Image
-//                                 src={props.picture}
-//                                 alt="윈도우 사진"
-//                                 style={imageStyle}
-//                                 layout="fill"
-//                                 priority={true}
-
-//                             />
-//                         </Link>
-//                     </div>
-
-//                     {/* right : name */}
-//                     <div className="flex flex-col">
-//                     <Link
-//                             href="/projects/1"
-//                         >
-//                         <div
-//                             style={{
-//                                 height: 150,
-//                                 width: 180,
-//                                 position: 'relative',
-//                             }}
-//                         >
-//                             <h1>{props.name}</h1>
-//                             <h2>{props.description}</h2>
-//                         </div>
-
-//                             <div
-//                                 className="items-center rounded-2xl border bg-white"
-//                                 style={{
-//                                     height: 160,
-//                                     width: 180,
-//                                     position: 'relative',
-//                                 }}
-//                             >
-//                                 dskflsd
-//                             </div>
-//                         </Link>
-//                         <div></div>
-//                     </div>
-//                 </div>
-//             ) : (
-//                 <div className="flex flex-row-reverse gap-6 ">
-//                 {/* left : image */}
-//                 <div
-//                     style={{
-//                         height: 310,
-//                         width: 220,
-//                         position: 'relative',
-//                     }}
-//                 >
-//                     <Link href="/projects/1">
-//                         <Image
-//                             src={props.picture}
-//                             //width={250}
-//                             //height={400}
-//                             alt="윈도우 사진"
-//                             style={imageStyle}
-//                             layout="fill"
-//                             priority={true}
-//                         />
-//                     </Link>
-//                 </div>
-
-//                 {/* right : name */}
-//                 <div className="flex flex-col">
-//                 <Link
-//                         href="/projects/1"
-//                     >
-//                     <div
-//                         style={{
-//                             height: 150,
-//                             width: 180,
-//                             position: 'relative',
-//                         }}
-//                     >
-//                         <h1>{props.name}</h1>
-//                         <h2>{props.description}</h2>
-//                     </div>
-
-//                         <div
-//                             className="items-center rounded-2xl border bg-white"
-//                             style={{
-//                                 height: 160,
-//                                 width: 180,
-//                                 position: 'relative',
-//                             }}
-//                         >
-//                             dskflsd
-//                         </div>
-//                     </Link>
-//                     <div></div>
-//                 </div>
-//             </div>
-
-//   )}
-//         </main>
-
-//     )
-// }
