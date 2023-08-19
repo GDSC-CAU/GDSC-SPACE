ddbf147a3de09eec8f50743a643a37c347be2132
yymin1022 <yymin1022@gmail.com>
2023-08-20 02:11:30 +0900

    feat: Integrated Legacy Member Interface to Newer

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,48 +1,56 @@
-import { Member } from '../../interfaces/common'
+import { MEMBER_DATA } from '../../interfaces/common'
 import { MemberCard } from './_components'
 
 export default function MemberList() {
-    const bulkMembers: Array<Member> = [
+    const bulkMembers: Array<MEMBER_DATA> = [
         {
-            name: '유용민',
-            gender: 'Male',
-            nickname: 'DEV.LR',
-            role: 'Lead',
-            year: '2기',
-            position: 'Cloud Server / flutter',
-            introduction: '안녕하세요, GDSC 3기 리더 유용민입니다. 잘 부탁드립니다:)',
-            github: 'github.com/yymin1022',
-            email: 'yymin1022@gmail.com',
-            instagram: '@useful_min',
+            MEMBER_INSTAGRAM: 'instagram',
+            MEMBER_IMAGE: '',
+            MEMBER_LINK_BEHANCE: '',
+            MEMBER_NAME: '유용민',
+            MEMBER_GENDER: 'Male',
+            MEMBER_NICKNAME: 'DEV.LR',
+            MEMBER_ROLE: 'Lead',
+            MEMBER_YEAR: '2기',
+            MEMBER_POSITION: 'Cloud Server / flutter',
+            MEMBER_COMMENT: '안녕하세요, GDSC 3기 리더 유용민입니다. 잘 부탁드립니다:)',
+            MEMBER_LINK_GITHUB: 'github.com/yymin1022',
+            MEMBER_EMAIL: 'yymin1022@gmail.com',
         },
         {
-            name: '장준성',
-            gender: 'Male',
-            nickname: '단팥초',
-            role: 'Core',
-            year: '2기',
-            position: 'Front-End',
-            introduction: '안녕하세요~!~!',
-            github: 'github.com/danpachodanpachodanpacho',
-            email: 'danpa725@cau.ac.kr',
+            MEMBER_INSTAGRAM: 'instagram',
+            MEMBER_IMAGE: '',
+            MEMBER_LINK_BEHANCE: '',
+            MEMBER_NAME: '장준성',
+            MEMBER_GENDER: 'Male',
+            MEMBER_NICKNAME: '단팥초',
+            MEMBER_ROLE: 'Core',
+            MEMBER_YEAR: '2기',
+            MEMBER_POSITION: 'Front-End',
+            MEMBER_COMMENT: '안녕하세요~!~!',
+            MEMBER_LINK_GITHUB: 'github.com/danpachodanpachodanpacho',
+            MEMBER_EMAIL: 'danpa725@cau.ac.kr',
         },
         {
-            name: '박지우',
-            gender: 'Female',
-            role: 'Member',
-            year: '3기',
-            nickname: 'Jiwoo',
-            position: 'Front-End',
-            introduction: '안녕하세용!!',
-            github: 'github.com/jujuredt',
-            email: 'juju.0_0._',
+            MEMBER_INSTAGRAM: 'instagram',
+            MEMBER_IMAGE: '',
+            MEMBER_LINK_BEHANCE: '',
+            MEMBER_NAME: '박지우',
+            MEMBER_GENDER: 'Female',
+            MEMBER_ROLE: 'Member',
+            MEMBER_YEAR: '3기',
+            MEMBER_NICKNAME: 'Jiwoo',
+            MEMBER_POSITION: 'Front-End',
+            MEMBER_COMMENT: '안녕하세용!!',
+            MEMBER_LINK_GITHUB: 'github.com/jujuredt',
+            MEMBER_EMAIL: 'juju.0_0._',
         },
     ]
 
     return (
         <main className="flex h-full w-full flex-row items-center justify-between gap-10">
             {bulkMembers.map((member) => (
-                <MemberCard member={member} key={`${member.name}-${member.position}`} />
+                <MemberCard member={member} key={`${member.MEMBER_NAME}-${member.MEMBER_POSITION}`} />
             ))}
         </main>
     )
