f8377f603af4008b5a3a9b4088ff11bba930684d
yymin1022 <yymin1022@gmail.com>
2023-08-20 02:15:27 +0900

    feat: Integrated Legacy Event Interface to Newer

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,14 +1,14 @@
-import { Event } from '../../interfaces/common'
+import { EVENT_META } from '../../interfaces/common'
 import { EventCard } from './_components/eventCard'
 
 export default function EventList() {
-    const Event: Event = {
-        id: '3',
-        title: 'GDSC Job Fair 2023',
-        content: 'by 대한민국 인공지능위크 2023',
-        date: 'Jul 15, 2023',
-        category: 'Association',
-        image: '/events/cardtest.png',
+    const Event: EVENT_META = {
+        EVENT_ID: '3',
+        EVENT_TITLE: 'GDSC Job Fair 2023',
+        EVENT_DESCRIPTION: 'by 대한민국 인공지능위크 2023',
+        EVENT_DATE: 'Jul 15, 2023',
+        EVENT_TYPE: 'Association',
+        EVENT_THUMBNAIL: '/events/cardtest.png',
     }
     return (
         <main className="flex min-w-[50%] flex-col items-center justify-center">
