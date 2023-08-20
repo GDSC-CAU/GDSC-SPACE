d74a2641b5aa31fd1280420aba312ff0a1f8e8bd
Danpa_cho <danpa725@cau.ac.kr>
2023-08-20 14:27:04 +0900

    refactor: member page and bulk data generation

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,140 +1,70 @@
 'use client'
 
-import { useRef, useState } from 'react'
+import { useState } from 'react'
 import type { MEMBER_DATA } from '../../interfaces/common'
-import { MemberCard } from './_components'
+import { MemberHorizontalScrollView } from './_components'
+
+const generateBulkMembers = (count: number): Array<MEMBER_DATA> => {
+    const bulkMembers: Array<MEMBER_DATA> = Array.from(
+        {
+            length: count,
+        },
+        (_, i) =>
+            ({
+                MEMBER_INSTAGRAM: 'instagram',
+                MEMBER_IMAGE: '',
+                MEMBER_LINK_BEHANCE: '',
+                MEMBER_NAME: `유용민${i}`,
+                MEMBER_GENDER: 'Male',
+                MEMBER_NICKNAME: 'DEV.LR',
+                MEMBER_ROLE: 'Lead',
+                MEMBER_YEAR: `${Math.floor(Math.random() * 3) + 1}기` as MEMBER_DATA['MEMBER_YEAR'],
+                MEMBER_POSITION: 'Cloud Server / flutter',
+                MEMBER_COMMENT: '안녕하세요, GDSC 3기 리더 유용민입니다. 잘 부탁드립니다:)',
+                MEMBER_LINK_GITHUB: 'github.com/yymin1022',
+                MEMBER_EMAIL: 'yymin1022@gmail.com',
+            } as MEMBER_DATA)
+    )
+    return bulkMembers
+}
+
+const bulkMembers: Array<MEMBER_DATA> = generateBulkMembers(300)
 
 export default function MemberList() {
-    const bulkMembers: Array<MEMBER_DATA> = [
-        {
-            MEMBER_INSTAGRAM: 'instagram',
-            MEMBER_IMAGE: '',
-            MEMBER_LINK_BEHANCE: '',
-            MEMBER_NAME: '유용민',
-            MEMBER_GENDER: 'Male',
-            MEMBER_NICKNAME: 'DEV.LR',
-            MEMBER_ROLE: 'Lead',
-            MEMBER_YEAR: '2기',
-            MEMBER_POSITION: 'Cloud Server / flutter',
-            MEMBER_COMMENT: '안녕하세요, GDSC 3기 리더 유용민입니다. 잘 부탁드립니다:)',
-            MEMBER_LINK_GITHUB: 'github.com/yymin1022',
-            MEMBER_EMAIL: 'yymin1022@gmail.com',
-        },
-        {
-            MEMBER_INSTAGRAM: 'instagram',
-            MEMBER_IMAGE: '',
-            MEMBER_LINK_BEHANCE: '',
-            MEMBER_NAME: '장준성',
-            MEMBER_GENDER: 'Male',
-            MEMBER_NICKNAME: '단팥초',
-            MEMBER_ROLE: 'Core',
-            MEMBER_YEAR: '2기',
-            MEMBER_POSITION: 'Front-End',
-            MEMBER_COMMENT: '안녕하세요~!~!',
-            MEMBER_LINK_GITHUB: 'github.com/danpachodanpachodanpacho',
-            MEMBER_EMAIL: 'danpa725@cau.ac.kr',
-        },
-        {
-            MEMBER_INSTAGRAM: 'instagram',
-            MEMBER_IMAGE: '',
-            MEMBER_LINK_BEHANCE: '',
-            MEMBER_NAME: '박지우',
-            MEMBER_GENDER: 'Female',
-            MEMBER_ROLE: 'Member',
-            MEMBER_YEAR: '3기',
-            MEMBER_NICKNAME: 'Jiwoo',
-            MEMBER_POSITION: 'Front-End',
-            MEMBER_COMMENT: '안녕하세용!!',
-            MEMBER_LINK_GITHUB: 'github.com/jujuredt',
-            MEMBER_EMAIL: 'juju.0_0._',
-        },
-    ]
-
-    const [selectedYear, setSelectedYear] = useState<string>('1st')
-
-    const renderMembersByYear = (year: string) => {
-        const filteredMembers = members.filter((member) => member.year === year)
-        const chunkedMembers = chunkArray(filteredMembers, 6)
-        return chunkedMembers.map((chunk, rowIndex) => (
-            <div
-                key={rowIndex}
-                className="mb-10 pb-9 pl-2 pt-2 scrollbar-hide"
-                style={{
-                    display: 'flex',
-                    overflowX: 'auto',
-                    overflowY: 'hidden',
-                }}
-            >
-                {chunk.map((member, index) => (
-                    <div
-                        key={index}
-                        style={{
-                            flex: '0 0 16.666%',
-                            marginRight: '1rem',
-                            height: '260px',
-                        }}
-                    >
-                        <MemberCard member={member} key={`${member.name}-${member.position}`} />
-                    </div>
-                ))}
-            </div>
-        ))
-    }
-    /**
-     <div className="flex h-full w-full flex-row items-center justify-between gap-10">
-        {bulkMembers.map((member) => (
-            <MemberCard member={member} key={`${member.MEMBER_NAME}-${member.MEMBER_POSITION}`} />
-        ))}
-    </div>
-    */
-
-    const chunkArray = (arr: any[], size: number) => {
-        const chunkedArray = []
-        for (let i = 0; i < arr.length; i += size) {
-            chunkedArray.push(arr.slice(i, i + size))
-        }
-        return chunkedArray
-    }
-
-    const scrollContainerRef = useRef<HTMLDivElement | null>(null)
-
-    const handleScroll = (event: React.WheelEvent<HTMLDivElement>) => {
-        if (scrollContainerRef.current) {
-            const delta = event.deltaY
-            scrollContainerRef.current.scrollLeft += delta
-            event.preventDefault()
-        }
-    }
+    const [selectedYear, setSelectedYear] = useState<MEMBER_DATA['MEMBER_YEAR']>('1기')
+    const targetMemberList = bulkMembers.filter(({ MEMBER_YEAR }) => selectedYear === MEMBER_YEAR)
 
     return (
         <main className="flex h-full w-full flex-col p-5">
             <div className="mb-10 flex">
                 <button
-                    onClick={() => setSelectedYear('1st')}
+                    type="button"
+                    aria-label="1st year members"
+                    onClick={() => setSelectedYear('1기')}
                     className={`mr-10 h-10 w-16 rounded-full bg-primary-blue p-2 font-bold text-white hover:underline`}
                 >
                     1st
                 </button>
                 <button
-                    onClick={() => setSelectedYear('2nd')}
+                    type="button"
+                    aria-label="2nd year members"
+                    onClick={() => setSelectedYear('2기')}
                     className={`mr-10 h-10 w-16 rounded-full bg-primary-blue p-2 font-bold text-white hover:underline`}
                 >
                     2nd
                 </button>
                 <button
-                    onClick={() => setSelectedYear('3rd')}
+                    type="button"
+                    aria-label="3rd year members"
+                    onClick={() => setSelectedYear('3기')}
                     className={`h-10 w-16 rounded-full bg-primary-blue p-2 font-bold text-white hover:underline`}
                 >
                     3rd
                 </button>
             </div>
-            <div
-                ref={scrollContainerRef}
-                className="overflow-x-auto overflow-y-hidden"
-                onWheel={handleScroll}
-                style={{ display: 'flex', flexDirection: 'column' }}
-            >
-                {renderMembersByYear(selectedYear)}
+
+            <div className="flex flex-col py-4">
+                <MemberHorizontalScrollView members={targetMemberList} />
             </div>
         </main>
     )
