e07a5b9fbeff51d6040b8cb2628a21ddd9683d02
Danpa_cho <danpa725@cau.ac.kr>
2023-08-21 19:00:56 +0900

    design: change ratio of image and text element at thumbnail card component

--- src/components/common/ThumbnailCard.tsx
+++ src/components/common/ThumbnailCard.tsx
@@ -30,13 +30,13 @@ export function ThumbnailCard<ThumbnailMeta>({
             className={`${twClass} group flex w-64 flex-col gap-0 rounded-[0.65rem] border-2 border-transparent bg-transparent transition-colors duration-300 hover:border-primary-blue`}
         >
             <FillImage
-                containerClass="flex h-44 w-full items-center justify-end overflow-hidden rounded-t-lg"
+                containerClass="flex h-[12rem] w-full items-center justify-end overflow-hidden rounded-t-lg"
                 imageClass="h-full w-full duration-300 scale-110 brightness-100 transition-all group-hover:brightness-110 ease-in-out group-hover:scale-105 transform-gpu"
                 src={thumbnailMeta.imageSrc}
                 alt={`${thumbnailMeta.title}-image`}
             />
 
-            <section className="flex h-40 w-full flex-col items-start justify-between overflow-y-hidden rounded-b-lg bg-black p-5">
+            <section className="flex h-44 w-full flex-col items-start justify-between overflow-y-hidden rounded-b-lg bg-black p-5">
                 <div>
                     <ul className="flex flex-row gap-0 pb-2 font-kor text-xs font-light">
                         <li className="max-w-[6rem] truncate">{thumbnailMeta.cardType}</li>
