ecc83376ec9fa10c61f9439d0c56c86a7a4a24aa
Danpa_cho <danpa725@cau.ac.kr>
2023-08-21 16:01:22 +0900

    refactor: intro icon size and export path

--- src/components/icons/IntroIcon.tsx
+++ src/components/icons/IntroIcon.tsx
@@ -1,9 +1,8 @@
 import * as React from 'react'
-//import { SVGProps } from 'react'
 
 const SvgIntro = () => (
     <svg width="180" height="180" viewBox="0 0 230 322" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <rect opacity="0.4" width="56" height="56" transform="matrix(1 0 0 -1 45 138)" fill="#1753F1" />
+        <rect opacity="0.4" width="46" height="46" transform="matrix(1 0 0 -1 45 138)" fill="#1753F1" />
         <rect opacity="0.4" width="46" height="46" transform="matrix(1 0 0 -1 91 184)" fill="#1753F1" />
         <rect opacity="0.4" width="46" height="46" transform="matrix(1 0 0 -1 45 230)" fill="#1753F1" />
         <rect opacity="0.4" width="46" height="46" transform="matrix(1 0 0 -1 184 47)" fill="#1753F1" />
