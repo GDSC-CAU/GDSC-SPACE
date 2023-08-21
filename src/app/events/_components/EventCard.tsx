db556ed7990c2dfe1ed6680480cfe6e7f1658e3c
Danpa_cho <danpa725@cau.ac.kr>
2023-08-21 18:51:25 +0900

    rename: eventCard to pascal case

---
+++ src/app/events/_components/EventCard.tsx
@@ -0,0 +1,19 @@
+import { EVENT_META } from 'src/interfaces/common'
+import { ThumbnailCard } from '~/components/common'
+
+export const EventCard = ({ event }: { event: EVENT_META }) => {
+    return (
+        <ThumbnailCard
+            twClass="animate-rise duration-500"
+            meta={event}
+            href={`/events/${event.EVENT_ID}`}
+            transformer={(event) => ({
+                date: event.EVENT_DATE,
+                description: event.EVENT_DESCRIPTION,
+                imageSrc: event.EVENT_THUMBNAIL,
+                title: event.EVENT_TITLE,
+                cardType: event.EVENT_TYPE,
+            })}
+        />
+    )
+}
