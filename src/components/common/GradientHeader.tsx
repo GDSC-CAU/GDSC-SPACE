7a7c94fbc8d9776f0b397066492a35cb0bfc4ac4
Danpa_cho <danpa725@cau.ac.kr>
2023-08-21 19:09:54 +0900

    design: add z-10 property for gradient header

--- src/components/common/GradientHeader.tsx
+++ src/components/common/GradientHeader.tsx
@@ -8,7 +8,7 @@ export const GradientHeader = ({
     children,
 }: React.PropsWithChildren<GradientHeaderProps>) => (
     <h1
-        className={`${size} ${twClass} bg-gradient-to-b from-primary-blue to-white bg-clip-text font-eng font-bold text-transparent`}
+        className={`${size} ${twClass} z-10 bg-gradient-to-b from-primary-blue to-white bg-clip-text font-eng font-bold text-transparent`}
     >
         {children}
     </h1>
