b4d2a5dafbc38c5d1781e7cab88258bdb77787c4
Danpa_cho <danpa725@cau.ac.kr>
2023-08-23 13:06:53 +0900

    refactor: define project card component prop interface and fix sub image src path

--- src/app/_components/ProjectCard.tsx
+++ src/app/_components/ProjectCard.tsx
@@ -4,23 +4,11 @@ import { useEffect, useRef, useState } from 'react'
 import { MAIN_PROJECT_DATA } from 'src/interfaces/common'
 import { FillImage } from '~/components/common'
 
-export default function ProjectCard({
-    isReverse,
-    projectData,
-}: {
+interface ProjectCardProps {
     isReverse: boolean
-    projectData: MAIN_PROJECT_DATA | undefined
-}) {
-    const project: MAIN_PROJECT_DATA = {
-        PROJECT_TITLE: 'Wiro',
-        PROJECT_ID: 'PROJECT_ID',
-        PROJECT_IMAGE: 'https://cdn.pixabay.com/photo/2023/08/11/18/35/flowers-8184126_1280.jpg',
-        PROJECT_IMAGE_SUB: 'https://cdn.pixabay.com/photo/2023/08/05/15/15/waves-8171279_1280.jpg',
-        PROJECT_DESCRIPTION:
-            'GDSC CAU GDSC CAU GDSC CAU GDSC CAU GDSC CAU GDSC CAU GDSC CAU GDSC CAU GDSC CAU ' +
-            'GDSC CAU GDSC CAU GDSC CAU GDSC CAU GDSC CAU GDSC CAU GDSC CAU GDSC CAU GDSC CAU GDSC CAU GDSC CAU',
-        PROJECT_SUBTITLE: ': No more Lonely Death',
-    }
+    projectData: MAIN_PROJECT_DATA
+}
+export const ProjectCard = ({ isReverse, projectData }: ProjectCardProps) => {
     const textAlign = isReverse ? 'text-end' : 'text-start'
     const LCardMargin = isReverse ? 'ml-[20px]' : 'mr-[-20px]'
     const RCardMargin = isReverse ? 'ml-[20px]' : 'mr-[-20px]'
@@ -60,36 +48,36 @@ export default function ProjectCard({
     }, [])
 
     return (
-        <Link href={`/blog/${project.PROJECT_ID}`} className="group flex flex-row items-center" ref={ImageRef}>
+        <Link href={`/blog/${projectData.PROJECT_ID}`} className="group flex flex-row items-center" ref={ImageRef}>
             <FillImage
-                src={project.PROJECT_IMAGE}
-                alt={project.PROJECT_TITLE}
-                containerClass={`h-[480px] w-[420px] overflow-hidden rounded-xl transition-all duration-1000 ease-in-out transform-gpu ${LCardMargin} ${
+                src={projectData.PROJECT_IMAGE}
+                alt={projectData.PROJECT_TITLE}
+                containerClass={`h-[480px] w-[420px] overflow-hidden rounded-xl transition-all duration-1000 ease-in-out ${LCardMargin} ${
                     isVisible ? LScrollTo : LScrollFrom
                 }`}
                 imageClass="rounded-xl group-hover:scale-110 group-hover:brightness-110 transition-all duration-500"
             />
 
             <div
-                className={`h-[480px] w-[420px] ${RCardMargin} flex flex-col transition-all duration-1000 ease-in-out transform-gpu ${
+                className={`h-[480px] w-[420px] ${RCardMargin} flex flex-col transition-all duration-1000 ease-in-out ${
                     isVisible ? RScrollTo : RScrollFrom
                 }`}
             >
                 <section className={`flex h-full w-full flex-col items-end justify-start`}>
-                    <h1 className={`-mt-2 w-full text-6xl font-extrabold text-white ${textAlign}`}>
-                        {project.PROJECT_TITLE}
+                    <h1 className={`w-full text-6xl font-extrabold text-white ${textAlign}`}>
+                        {projectData.PROJECT_TITLE}
                     </h1>
                     <h2 className={`mb-10 w-full text-3xl font-bold text-white ${textAlign}`}>
-                        {project.PROJECT_SUBTITLE}
+                        {projectData.PROJECT_SUBTITLE}
                     </h2>
-                    <p className={`w-full text-sm ${textAlign}`}>{project.PROJECT_DESCRIPTION}</p>
+                    <p className={`w-full text-sm ${textAlign}`}>{projectData.PROJECT_DESCRIPTION}</p>
                 </section>
 
                 <FillImage
+                    src={projectData.PROJECT_IMAGE_SUB}
+                    alt={projectData.PROJECT_SUBTITLE}
                     containerClass="h-[280px] w-[420px]"
                     imageClass="rounded-xl"
-                    src={project.PROJECT_IMAGE}
-                    alt="pixabay picture3"
                 />
             </div>
         </Link>
