efdbd0384b6f7109e0db9a486da69cb97a7cc591
Danpa_cho <danpa725@cau.ac.kr>
2023-08-23 22:36:28 +0900

    feat: make mentor benefit card

---
+++ src/app/_components/benefits/mentor/Mentor.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { Mentor as MentorIcon } from '~/components/icons'
+import { BenefitCard } from '../benefitCard'
+import { useTilt } from './useTilt'
+
+export const Mentor = () => {
+    const { listeners, setTiltRef, style } = useTilt<HTMLDivElement>({
+        shadowBlurLevel: 4.5,
+        shadowColor: 'rgba(0, 0, 0, 0.2)',
+        shadowTiltLevel: 1,
+    })
+
+    return (
+        <div ref={setTiltRef} {...listeners} className="col-start-4 col-end-8 row-auto">
+            <BenefitCard white colAlign="around" rowAlign="center">
+                <BenefitCard.Header>Get</BenefitCard.Header>
+                <div style={style}>
+                    <MentorIcon className="transition-transform duration-500 scale-75 hover:scale-90" />
+                </div>
+                <BenefitCard.Header>a mentor.</BenefitCard.Header>
+            </BenefitCard>
+        </div>
+    )
+}
