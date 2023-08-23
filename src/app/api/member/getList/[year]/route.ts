fcd0dab435ce529238475834547ca032e1505a10
yymin1022 <yymin1022@gmail.com>
2023-08-24 01:28:55 +0900

    feat: Added Dummy Response of Member List API

--- src/app/api/member/getList/[year]/route.ts
+++ src/app/api/member/getList/[year]/route.ts
@@ -1,5 +1,36 @@
 import { NextRequest, NextResponse } from 'next/server'
+import { API_MAIN_MEMBERS, API_MEMBER_LIST, API_RESPONSE, MEMBER_DATA } from '~/src/interfaces/common'
 
 export async function GET(request: NextRequest, { params }: { params: { year: string } }) {
-    return NextResponse.json({ date: 'Member List', year: params.year }, { status: 200 })
+    const dummyMemberData: MEMBER_DATA = {
+        MEMBER_COMMENT: '안녕하세요, GDSC 3기 리드 유용민입니다. 잘 부탁드립니다:)',
+        MEMBER_EMAIL: 'yymin1022@gmail.com',
+        MEMBER_GENDER: 'Male',
+        MEMBER_INSTAGRAM: 'instagram',
+        MEMBER_IMAGE: '',
+        MEMBER_LINK_BEHANCE: '',
+        MEMBER_LINK_GITHUB: 'github.com/yymin1022',
+        MEMBER_NAME: `유용민`,
+        MEMBER_NICKNAME: 'DEV.LR',
+        MEMBER_POSITION: 'Cloud Server / flutter',
+        MEMBER_ROLE: 'Lead',
+        MEMBER_YEAR: `${params.year}기` as MEMBER_DATA['MEMBER_YEAR'],
+    }
+
+    const dummyMemberList: API_MEMBER_LIST = {
+        MEMBER_CNT: 25,
+        MEMBER_LIST: [],
+    }
+
+    for (let i = 0; i < 4; i++) {
+        dummyMemberList.MEMBER_LIST.push(dummyMemberData)
+    }
+
+    const apiResult: API_RESPONSE = {
+        RESULT_CODE: 200,
+        RESULT_MSG: 'Success',
+        RESULT_DATA: dummyMemberList,
+    }
+
+    return NextResponse.json(apiResult, { status: 200 })
 }
