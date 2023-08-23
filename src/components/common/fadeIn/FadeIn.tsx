37ea16be12f60de71e9ddbb5228835f840f76a95
Danpa_cho <danpa725@cau.ac.kr>
2023-08-23 23:21:59 +0900

    feat: add `FadeIn` animation component

---
+++ src/components/common/fadeIn/FadeIn.tsx
@@ -0,0 +1,20 @@
+import { TailwindComponent } from '../tailwind'
+import { useVisibility, UseVisibilityProps } from './useVisibility'
+
+interface FadeInProps extends UseVisibilityProps, TailwindComponent {
+    from: string
+    to: string
+}
+
+export const FadeIn = ({ from, to, observerOption, children, twClass }: React.PropsWithChildren<FadeInProps>) => {
+    const { isVisible, setVisibilityRef } = useVisibility<HTMLDivElement>({ observerOption })
+
+    const fromStyle = `${from} opacity-0`
+    const toStyle = `${to} opacity-100`
+
+    return (
+        <div ref={setVisibilityRef} className={`${isVisible ? toStyle : fromStyle} ${twClass} transition-all`}>
+            {children}
+        </div>
+    )
+}
