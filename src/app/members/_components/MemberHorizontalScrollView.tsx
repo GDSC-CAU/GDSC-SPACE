17eba8c66e3f4a9aef05d1f43eadf8527b498b37
jujured <pjiww4@gmail.com>
2023-08-26 03:21:39 +0900

    feat : 그림자 위치 고정

--- src/app/members/_components/MemberHorizontalScrollView.tsx
+++ src/app/members/_components/MemberHorizontalScrollView.tsx
@@ -12,14 +12,11 @@ function divideArrayIntoChunk<T>(arr: T[], chunkSize: number) {
 
 const GradientBackground = ({ rowIndex }: { rowIndex: number }) => (
     <div
-        className={`absolute inset-0 ${
+        className={`z-10 px-52${
             rowIndex % 2 === 0
-                ? 'flex-end via-darkgray w-1/12 bg-gradient-to-l from-primary-darkgray to-transparent'
-                : 'via-darkgray w-1/12 bg-gradient-to-r from-primary-darkgray to-transparent'
+                ? 'absolute right-0 h-[18rem] w-[4rem] bg-gradient-to-l from-primary-hotpink to-transparent'
+                : 'absolute left-0 h-[18rem] w-[4rem] bg-gradient-to-r from-primary-blue to-transparent'
         }`}
-        style={{
-            zIndex: 1,
-        }}
     />
 )
 
@@ -31,16 +28,17 @@ export const MemberHorizontalScrollView = ({ members }: RenderMemberByYearProps)
     const dividedMembers = divideArrayIntoChunk(members, 6)
 
     return dividedMembers.map((memberChunk, rowIndex) => (
-        <div
-            key={rowIndex}
-            className={`relative flex flex-row gap-8 overflow-x-auto overflow-y-hidden px-4 py-7 scrollbar-hide ${
-                rowIndex % 2 === 0 ? 'flex-row' : 'flex-row-reverse'
-            }`}
-        >
+        <div key={rowIndex} className="relative">
+            <div
+                className={`flex flex-row gap-8 overflow-x-auto overflow-y-hidden px-4 py-7 scrollbar-hide ${
+                    rowIndex % 2 === 0 ? 'flex-row' : 'flex-row-reverse'
+                }`}
+            >
+                {memberChunk.map((member) => (
+                    <MemberCard key={`${member.MEMBER_NAME}-${member.MEMBER_POSITION}`} member={member} />
+                ))}
+            </div>
             <GradientBackground rowIndex={rowIndex} />
-            {memberChunk.map((member) => (
-                <MemberCard key={`${member.MEMBER_NAME}-${member.MEMBER_POSITION}`} member={member} />
-            ))}
         </div>
     ))
 }
