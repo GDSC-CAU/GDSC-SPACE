e3f096116812ada667f4cd68167448fdcfb4ea56
Danpa_cho <danpa725@cau.ac.kr>
2023-08-25 17:20:57 +0900

    feat: add timing control props at FadeIn common component

--- src/components/common/fadeIn/FadeIn.tsx
+++ src/components/common/fadeIn/FadeIn.tsx
@@ -10,29 +10,42 @@ const tailwindDuration = {
     '1000': 'duration-1000',
 } as const
 
-interface FadeInProps extends UseVisibilityProps, TailwindComponent {
-    from: string
-    to: string
+const tailwindAnimationTiming = {
+    'ease-in': 'ease-in',
+    'ease-out': 'ease-out',
+    'ease-in-out': 'ease-in-out',
+    'ease-linear': 'ease-linear',
+    ease: 'ease',
+} as const
+
+export interface FadeInProps extends UseVisibilityProps, TailwindComponent {
+    from?: string
+    to?: string
     duration?: keyof typeof tailwindDuration
+    animationTiming?: keyof typeof tailwindAnimationTiming
 }
 
 export const FadeIn = ({
     from,
     to,
-    duration = '500',
+    duration = '700',
     observerOption,
     children,
     twClass,
+    animationTiming = 'ease',
 }: React.PropsWithChildren<FadeInProps>) => {
     const { isVisible, setVisibilityRef } = useVisibility<HTMLDivElement>({ observerOption })
 
     const fromStyle = `${from} opacity-0`
     const toStyle = `${to} opacity-100`
 
+    const customizedAnimation = `${tailwindDuration[duration]} ${tailwindAnimationTiming[animationTiming]}`
     return (
         <div
             ref={setVisibilityRef}
-            className={`${isVisible ? toStyle : fromStyle} ${tailwindDuration[duration]} ${twClass} transition-all`}
+            className={`${
+                isVisible ? toStyle : fromStyle
+            } ${customizedAnimation} ${twClass} transition-all transform-gpu`}
         >
             {children}
         </div>
