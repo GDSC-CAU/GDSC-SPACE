3487e94607cea998f7cfd4a03b2da341d319d913
Danpa_cho <danpa725@cau.ac.kr>
2023-08-25 17:22:08 +0900

    design: make hover interaction at timeline card component

--- src/app/events/_components/TimeLineCard.tsx
+++ src/app/events/_components/TimeLineCard.tsx
@@ -1,26 +1,42 @@
+'use client'
+
 import { MAIN_TIMELINE_DATA } from 'src/interfaces/common'
+import { FadeIn } from '~/src/components/common'
 
 export const TimeLineCard = ({
     timeLine,
-    bg_color,
-    text_color,
+    bgColor,
+    textColor,
+    borderGroupColor,
 }: {
     timeLine: MAIN_TIMELINE_DATA
-    bg_color: string
-    text_color: string
+    bgColor: string
+    textColor: string
+    borderGroupColor: string
 }) => {
     return (
-        <section className="relative inset-0 z-10 h-[15rem] w-[25rem]">
-            <div
-                className={`flex h-[15rem] w-full items-start justify-start rounded-3xl ${bg_color} px-5 py-4 text-2xl text-black`}
-            >
-                {timeLine.TIMELINE_CARD_TITLE}
-            </div>
-            <div className="absolute inset-x-0 bottom-0 flex h-[11rem] w-full flex-col items-start justify-start rounded-t-3xl rounded-bl-lg rounded-br-3xl bg-black p-6">
-                <div className={`text-xl ${text_color} mb-10`}>{timeLine.TIMELINE_TITLE}</div>
-                <div className="text-xl text-white">{timeLine.TIMELINE_DATE}</div>
-                <div className="text-xl text-white">{timeLine.TIMELINE_DESCRIPTION}</div>
-            </div>
-        </section>
+        <FadeIn
+            observerOption={{
+                rootMargin: '-10px 0px 0px 0px',
+            }}
+            from="translate-x-full"
+            to="translate-x-0"
+            duration="700"
+        >
+            <section className="group relative inset-0 z-10 h-[13.5rem] w-[25rem]">
+                <h1
+                    className={`${bgColor} flex h-[13.5rem] w-full select-none items-start justify-start rounded-3xl px-5 py-4 font-eng text-lg text-black`}
+                >
+                    {timeLine.TIMELINE_CARD_TITLE}
+                </h1>
+                <ul
+                    className={`${borderGroupColor} absolute inset-x-0 bottom-0 flex h-[10rem] w-full flex-col items-start justify-start rounded-t-3xl rounded-bl-lg rounded-br-3xl border border-transparent bg-black p-5 transition-all duration-300 group-hover:shadow-2xl group-hover:brightness-125 group-hover:skew-x-2 group-hover:scale-105`}
+                >
+                    <h2 className={`${textColor} mb-10 select-none font-eng text-lg`}>{timeLine.TIMELINE_TITLE}</h2>
+                    <p className="select-none text-base font-light text-white">{timeLine.TIMELINE_DATE}</p>
+                    <p className="select-none text-base font-light text-white">{timeLine.TIMELINE_DESCRIPTION}</p>
+                </ul>
+            </section>
+        </FadeIn>
     )
 }
