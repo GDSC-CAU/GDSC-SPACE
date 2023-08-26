3cc94d9dd57d0ea94484e7a7608b9c86aadf39cc
yymin1022 <yymin1022@gmail.com>
2023-08-27 06:33:32 +0900

    feat: Update Notion Util

--- src/utils/NotionUtil.ts
+++ src/utils/NotionUtil.ts
@@ -2,12 +2,13 @@ import * as process from 'process'
 import { Client } from '@notionhq/client'
 
 const NOTION_API_KEY = process.env.NEXT_PUBLIC_NOTION_API_KEY
-const BUDGET_DB_ID = process.env.NEXT_PUBLIC_NOTION_MEMBER_DB
+const MEMBER_DB_ID = process.env.NEXT_PUBLIC_NOTION_MEMBER_DB
 
-let notionClient: Client | undefined = undefined
+const notionClient = new Client({ auth: NOTION_API_KEY })
 
-export const initNotion = () => {
-    if (notionClient !== undefined) {
-        notionClient = new Client({ auth: NOTION_API_KEY })
-    }
+export const getDatabase = async () => {
+    const notionData = await notionClient.databases.query({
+        database_id: MEMBER_DB_ID!,
+    })
+    return notionData.results
 }
