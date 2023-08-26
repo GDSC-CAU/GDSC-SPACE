1eef4fe45a16f23fc8994d00293f1973729de24d
yymin1022 <yymin1022@gmail.com>
2023-08-27 06:01:20 +0900

    feat: Update Notion Util

--- src/utils/NotionUtil.ts
+++ src/utils/NotionUtil.ts
@@ -1,3 +1,13 @@
-const initNotion = () => {
-    console.log('Hello, World!')
+import * as process from 'process'
+import { Client } from '@notionhq/client'
+
+const NOTION_API_KEY = process.env.NEXT_PUBLIC_NOTION_API_KEY
+const BUDGET_DB_ID = process.env.NEXT_PUBLIC_NOTION_MEMBER_DB
+
+let notionClient: Client | undefined = undefined
+
+export const initNotion = () => {
+    if (notionClient !== undefined) {
+        notionClient = new Client({ auth: NOTION_API_KEY })
+    }
 }
