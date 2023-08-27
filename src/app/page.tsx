21207e4e162132d2e626e4141735fd815e7839ad
Danpa_cho <danpa725@cau.ac.kr>
2023-08-27 20:43:32 +0900

    feat: finish desktop size main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
+import Image from 'next/image'
 import { API_MAIN_PROJECTS, API_MAIN_TIMELINES, API_RESPONSE } from 'src/interfaces/common'
 import { IntroIcon } from '~/components/icons'
-import { GradientHeader } from '../components/common'
+import { FadeIn, FadeInGradientHeader, GradientHeader } from '../components/common'
 import { Fetcher } from '../utils'
 import { ProjectCard } from './_components'
 import { Benefits } from './_components/benefits'
@@ -10,33 +11,13 @@ const mainFetcher = new Fetcher({
     baseUrl: 'http://localhost:3000',
 })
 
-const BulkBanner = () => {
+const WhoAreWeSection = () => {
     return (
-        <div className="flex h-72 w-full flex-col items-center justify-center gap-32 rounded-3xl border border-neutral-800 bg-theme-background py-40 text-sm">
-            <div className="flex flex-row gap-0">
-                <h1 className="bg-gradient-to-br from-primary-hotpink to-primary-hotpink bg-clip-text font-eng text-7xl font-bold text-transparent">
-                    G
-                </h1>
-                <h1 className="bg-gradient-to-br from-primary-yellow to-yellow-500 bg-clip-text font-eng text-7xl font-bold text-transparent">
-                    D
-                </h1>
-                <h1 className="bg-gradient-to-br from-primary-blue to-sky-500 bg-clip-text font-eng text-7xl font-bold text-transparent">
-                    S
-                </h1>
-                <h1 className="bg-gradient-to-br from-primary-teal to-green-500 bg-clip-text font-eng text-7xl font-bold text-transparent">
-                    C
-                </h1>
-            </div>
-            가상 Banner 영역입니다
-        </div>
-    )
-}
-
-const Intro = () => {
-    return (
-        <div className="relative ml-[calc(-13rem)] flex w-screen flex-col items-center justify-center bg-gradient-to-b from-black to-primary-darkgray py-20">
+        <div className="relative flex flex-col items-center justify-center py-20 pb-6">
             <div className="absolute left-1/2 top-1/4 h-64 w-64 rounded-full bg-primary-blue blur-3xl -translate-x-1/2 -translate-y-1/2"></div>
-            <GradientHeader twClass="mb-10 z-10">Who are we?</GradientHeader>
+            <FadeInGradientHeader id="about" twClass="mb-10 z-10">
+                Who are we?
+            </FadeInGradientHeader>
 
             <div className="z-10 flex flex-row items-start justify-center gap-20">
                 <div className="flex w-100 flex-col">
@@ -61,10 +42,55 @@ const Intro = () => {
                 </div>
             </div>
 
-            <div className="mt-5 flex h-32 w-32 animate-[pulse_3s_infinite_100ms] items-center justify-center">
+            <div className="mt-5 flex h-32 w-32 items-center justify-center">
                 <IntroIcon />
             </div>
         </div>
+    )
+}
+
+const Introduction = () => {
+    // -mt-20 for navbar height
+    return (
+        <section className="-mt-20 ml-[calc(-13rem)] flex w-screen select-none flex-col items-center justify-center bg-black">
+            <div className="relative h-fit w-fit">
+                <Image src="/banner.png" width={1920} height={1070} alt="GDSC CAU banner" priority quality={100} />
+
+                <div className="absolute inset-0 h-full w-full bg-gradient-to-b from-black/60 to-white/0" />
+
+                <div className="absolute left-1/2 top-1/2 flex flex-col items-center justify-center gap-8 -translate-x-1/2 -translate-y-1/2">
+                    <FadeIn from="translate-x-full scale-75" to="translate-x-0 scale-100" duration="1250">
+                        <h1 className="font-eng text-7xl font-semibold text-white">Google</h1>
+                    </FadeIn>
+                    <FadeIn from="-translate-x-full scale-75" to="translate-x-0 scale-100" duration="1250">
+                        <h1 className="font-eng text-7xl font-semibold text-white">Developer</h1>
+                    </FadeIn>
+                    <FadeIn from="translate-x-full scale-75" to="translate-x-0 scale-100" duration="1250">
+                        <h1 className="font-eng text-7xl font-semibold text-white">Student</h1>
+                    </FadeIn>
+                    <FadeIn from="-translate-x-full scale-75" to="translate-x-0 scale-100" duration="1250">
+                        <h1 className="font-eng text-7xl font-semibold text-white">Club</h1>
+                    </FadeIn>
+
+                    <FadeIn duration="1250">
+                        <p className="pt-10 text-xl font-light">Chung-Ang University</p>
+                    </FadeIn>
+                </div>
+            </div>
+
+            <div className="flex h-full w-full flex-col items-center justify-center gap-10 bg-gradient-to-b from-black to-primary-darkgray">
+                <FadeIn from="translate-y-10" to="translate-y-0" duration="1250">
+                    <div className="flex flex-col items-center justify-center pb-72 pt-56">
+                        <h2 className="bg-gradient-to-b from-primary-hotpink from-15% to-white bg-clip-text text-center font-eng text-4xl/normal font-medium text-transparent">
+                            From what you see
+                            <br></br>
+                            to what we want to see.
+                        </h2>
+                    </div>
+                </FadeIn>
+                <WhoAreWeSection />
+            </div>
+        </section>
     )
 }
 
@@ -85,57 +111,73 @@ const TimeLines = ({ MAIN_TIMELINE_LIST }: API_MAIN_TIMELINES) => {
         'text-primary-purple',
     ] as const
 
+    const border_group_colors = [
+        'group-hover:border-primary-hotpink/50',
+        'group-hover:border-primary-yellow/50',
+        'group-hover:border-primary-teal/50',
+        'group-hover:border-primary-blue/50',
+        'group-hover:border-primary-purple/50',
+    ] as const
+
     return (
-        <>
-            <div className="flex h-[60rem] w-full flex-row">
-                <div className="flex w-1/2 items-center justify-center">
-                    <GradientHeader>
-                        Here we go. <br /> What you will <br />
-                        experience.
-                    </GradientHeader>
-                </div>
-                <div className="mx-2 h-full w-[0.08rem] border-0 bg-gradient-to-b from-primary-blue to-white" />
-                <div className="flex h-full w-1/2 flex-col items-center justify-start gap-7 overflow-y-auto overflow-x-hidden p-3 pt-32 scrollbar-hide">
-                    {MAIN_TIMELINE_LIST.map((timeline, i) => (
-                        <TimeLineCard
-                            key={`${timeline.TIMELINE_TITLE}-${timeline.TIMELINE_DESCRIPTION}`}
-                            timeLine={timeline}
-                            bg_color={bg_colors[i % 5]}
-                            text_color={text_colors[i % 5]}
-                        />
-                    ))}
-                </div>
+        <section className="flex h-[52.5rem] w-full flex-row">
+            <div className="flex w-1/2 items-center justify-center">
+                <FadeInGradientHeader>
+                    Here we go. <br /> What you will <br />
+                    experience.
+                </FadeInGradientHeader>
             </div>
-        </>
+
+            <FadeIn
+                from="scale-y-0"
+                to="scale-y-100"
+                twClass="origin-top"
+                duration="1000"
+                animationTiming="ease-in-out"
+            >
+                <div className="ml-3.5 mt-0.5 h-full w-[0.08rem] border-0 bg-gradient-to-b from-primary-blue/40 to-white" />
+            </FadeIn>
+
+            <div className="flex h-full w-1/2 flex-col items-center justify-start gap-7 overflow-y-auto overflow-x-hidden p-3 pt-32 scrollbar-hide">
+                {MAIN_TIMELINE_LIST.map((timeline, i) => (
+                    <TimeLineCard
+                        key={`${timeline.TIMELINE_TITLE}-${timeline.TIMELINE_DESCRIPTION}`}
+                        timeLine={timeline}
+                        bgColor={bg_colors[i % 5]}
+                        textColor={text_colors[i % 5]}
+                        borderGroupColor={border_group_colors[i % 5]}
+                    />
+                ))}
+            </div>
+        </section>
     )
 }
 
 const Projects = ({ MAIN_PROJECT_LIST }: API_MAIN_PROJECTS) => {
     return (
-        <div className="mt-28 flex flex-col items-center justify-center gap-6">
-            <GradientHeader>Project</GradientHeader>
+        <section className="mt-28 flex flex-col items-center justify-center gap-6 pb-20">
+            <GradientHeader id="projects">Projects</GradientHeader>
             <div className="mb-12">우리는 이러한 문제점들을 읽어내고, 해결책을 탐구합니다!</div>
 
-            <div className="flex w-[1000px] flex-col items-center gap-10">
+            <div className="flex w-[1000px] flex-col items-center gap-20">
                 {MAIN_PROJECT_LIST.map((project, i) => (
                     <ProjectCard key={project.PROJECT_ID} isReverse={i % 2 === 0} projectData={project} />
                 ))}
             </div>
-        </div>
+        </section>
     )
 }
 
 export default async function Home() {
-    const timelines = (await mainFetcher.get<API_RESPONSE>('main/getTimelines')).RESULT_DATA as API_MAIN_TIMELINES
-    const mainProjects = (await mainFetcher.get<API_RESPONSE>('main/getProjects')).RESULT_DATA as API_MAIN_PROJECTS
+    const timelines = await mainFetcher.get<API_RESPONSE<API_MAIN_TIMELINES>>('main/getTimelines')
+    const mainProjects = await mainFetcher.get<API_RESPONSE<API_MAIN_PROJECTS>>('main/getProjects')
 
     return (
         <div className="h-full w-full">
-            <BulkBanner />
-            <Intro />
-            <TimeLines {...timelines} />
+            <Introduction />
+            <TimeLines {...timelines.RESULT_DATA} />
             <Benefits />
-            <Projects {...mainProjects} />
+            <Projects {...mainProjects.RESULT_DATA} />
         </div>
     )
 }
