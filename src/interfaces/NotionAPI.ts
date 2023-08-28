71dfb53787b8d44136e7147db6786ad4e6ef7418
yymin1022 <yymin1022@gmail.com>
2023-08-29 04:20:22 +0900

    feat: Added Notion API Interfaces

--- src/interfaces/NotionAPI.ts
+++ src/interfaces/NotionAPI.ts
@@ -38,3 +38,36 @@ export interface MemberTableRowData {
         }
     ]
 }
+
+export interface TimelineDBRow {
+    properties: {
+        Year: {
+            rich_text: [
+                {
+                    text: {
+                        content: string
+                    }
+                }
+            ]
+        }
+        Name: {
+            title: [
+                {
+                    text: {
+                        content: string
+                    }
+                }
+            ]
+        }
+    }
+}
+
+export interface TimelineTableRowData {
+    results: [
+        {
+            table_row: {
+                cells: [[{ plain_text: string }], [{ plain_text: string }]]
+            }
+        }
+    ]
+}
