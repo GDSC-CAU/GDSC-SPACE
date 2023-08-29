9aebc319a7d0e88d826c88dd569dd2020ae4793c
Danpa_cho <danpa725@cau.ac.kr>
2023-08-29 11:50:08 +0900

    design: update font size and weight at develop card

--- src/app/blog/_components/DevelopCard.tsx
+++ src/app/blog/_components/DevelopCard.tsx
@@ -4,18 +4,20 @@ export const DevelopCard = ({ article }: { article: BLOG_META }) => {
     return (
         <div className="flex w-full flex-col gap-y-1 rounded-2xl border border-black bg-black p-6 text-white transition-colors duration-300 hover:border-primary-blue md:gap-y-2 md:p-10">
             <div className="flex flex-row items-center justify-between">
-                <div className="flex flex-row gap-3 text-sm">
+                <ul className="flex flex-row gap-3 text-sm font-light">
                     {article.BLOG_TAG.map((tag) => (
-                        <div key={tag}> {tag} </div>
+                        <li key={tag}> {tag} </li>
                     ))}
-                </div>
-                <div>{article.BLOG_DATE}</div>
+                </ul>
+                <p className="text-sm font-light">{article.BLOG_DATE}</p>
             </div>
+
             <div className="flex flex-row items-center gap-5">
-                <div className="text-base font-semibold">{article.BLOG_TITLE}</div>
-                <div className="text-sm">by {article.BLOG_AUTHOR}</div>
+                <h1 className="text-base font-semibold">{article.BLOG_TITLE}</h1>
+                <h2 className="text-sm font-light">by {article.BLOG_AUTHOR}</h2>
             </div>
-            <div className="text-sm">{article.BLOG_DESCRIPTION}</div>
+
+            <p className="text-sm font-light">{article.BLOG_DESCRIPTION}</p>
         </div>
     )
 }
