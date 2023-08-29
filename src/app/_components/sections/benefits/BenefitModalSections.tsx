ce8b0a52adf4909bcd7cd5f1ae297ceb1f212574
Danpa_cho <danpa725@cau.ac.kr>
2023-08-29 16:17:03 +0900

    refactor: adjust mobile design at benefits modal card components

--- src/app/_components/sections/benefits/BenefitModalSections.tsx
+++ src/app/_components/sections/benefits/BenefitModalSections.tsx
@@ -14,7 +14,7 @@ export const ModalHeader = () => {
                 <BenefitModal.MainContent twClass="group col-start-1 xl:col-start-2 col-end-8 xl:col-end-7 row-auto overflow-hidden">
                     <FadeIn
                         observerOption={{
-                            rootMargin: '-500px 0px 0px 0px',
+                            rootMargin: '-100px 0px 0px 0px',
                         }}
                         twClass="w-full h-full"
                         from="-translate-y-1/2 scale-[2.5]"
@@ -23,8 +23,9 @@ export const ModalHeader = () => {
                         animationTiming="ease-out"
                     >
                         <CircleGradientHeader
-                            twClass="bg-transparent transition-all w-full h-[350px]"
-                            fontSize={1.4}
+                            id="desktop"
+                            twClass="md:block hidden bg-transparent transition-all w-full h-[350px]"
+                            textClassName="font-eng text-base md:text-lg"
                             fontWeight={500}
                             yOffset={-8.5}
                             radius={168.5}
@@ -32,11 +33,24 @@ export const ModalHeader = () => {
                         >
                             Show your ideas to the world.
                         </CircleGradientHeader>
+
+                        <CircleGradientHeader
+                            id="mobile"
+                            twClass="md:hidden block bg-transparent transition-all w-full h-[300px]"
+                            textClassName="font-eng text-[1.4rem]"
+                            fontWeight={500}
+                            yOffset={-30.5}
+                            radius={50.5}
+                            letterSpacing={0.5}
+                        >
+                            Show your ideas
+                        </CircleGradientHeader>
                     </FadeIn>
+
                     <div className="absolute left-1/2 top-1/4 -translate-x-1/2">
                         <FadeIn
                             observerOption={{
-                                rootMargin: '-500px 0px 0px 0px',
+                                rootMargin: '-100px 0px 0px 0px',
                             }}
                             twClass="w-full h-full"
                             duration="1000"
@@ -44,21 +58,21 @@ export const ModalHeader = () => {
                             from="scale-0"
                             to="scale-100"
                         >
-                            <Earth className="animate-spin-z-32" />
-                            <EarthEye className="absolute bottom-1/2 left-2/3 scale-150" />
+                            <Earth className="animate-spin-z-32 -translate-y-8 scale-[0.8] md:translate-y-0 md:scale-100" />
+                            <EarthEye className="absolute bottom-56 right-28 -translate-y-8 scale-90 md:bottom-1/2 md:left-2/3 md:translate-y-0 md:scale-150" />
                         </FadeIn>
                     </div>
                 </BenefitModal.MainContent>
 
                 <BenefitModal.ModalContent>
                     <BenefitCard.Description>
-                        <BenefitCard.Description.Eng>
+                        <BenefitCard.Description.Eng twClass="text-start md:text-start">
                             In the GDSC community, We plan and execute events by our self. Develop your leadership,
-                            cooperate with co-workers and show your gorgeous ideas!
+                            cooperate with co-workers and show your gorgeous ideas.
                         </BenefitCard.Description.Eng>
-                        <BenefitCard.Description.Kor>
+                        <BenefitCard.Description.Kor twClass="text-start md:text-start">
                             GDSC 커뮤니티에서는 학생들이 스스로 행사를 기획하고 실행합니다. 팀워크와 리더십을 키우고,
-                            자신의 아이디어를 함께 실현해보세요!
+                            자신의 아이디어를 함께 실현해보세요.
                         </BenefitCard.Description.Kor>
                     </BenefitCard.Description>
                 </BenefitModal.ModalContent>
@@ -75,25 +89,33 @@ export const ModalFooter = () => {
                     gray: true,
                 }}
                 white
-                flexDirection="row"
+                flexDirection="col"
                 rowAlign="center"
                 colAlign="around"
-                twClass="col-start-1 xl:col-start-2 gap-8 col-end-8 xl:col-end-7 row-auto"
+                twClass="md:flex-row-reverse md:col-start-1 xl:col-start-2 md:gap-8 md:col-end-8 xl:col-end-7"
             >
-                <BenefitCard.Header>Take the opportunity to grow.</BenefitCard.Header>
+                <Image
+                    src="/opportunity.png"
+                    width={350 * 0.75}
+                    height={368.98 * 0.75}
+                    className="mt-6 pb-0 scale-90 md:mt-0 md:pb-10 md:scale-100"
+                    alt="get opportunity"
+                />
 
-                <Image src="/opportunity.png" width={350 * 0.75} height={368.98 * 0.75} alt="get opportunity" />
+                <BenefitCard.Header twClass="mt-6 md:mt-0" center>
+                    Take the opportunity to grow.
+                </BenefitCard.Header>
             </BenefitModal.MainContent>
 
             <BenefitModal.ModalContent>
                 <BenefitCard.Description>
-                    <BenefitCard.Description.Eng>
-                        You will participate collaboration events such as GDSC Job fair and Programming Contest! Find
-                        great opportunities and gain various experiences!
+                    <BenefitCard.Description.Eng twClass="text-start md:text-start">
+                        You will participate collaboration events such as GDSC Job fair and Programming Contest. Find
+                        great opportunities and gain various experiences.
                     </BenefitCard.Description.Eng>
-                    <BenefitCard.Description.Kor>
-                        GDSC Job fair, Programming Contest와 같은 콜라보레이션 이벤트가 진행 될 수 있어요! 멋진 기회를
-                        찾고 다양한 경험을 쌓아보세요!
+                    <BenefitCard.Description.Kor twClass="text-start md:text-start">
+                        GDSC Job fair, Programming Contest와 같은 콜라보레이션 이벤트가 진행 될 수 있습니다. 멋진 기회를
+                        찾고 다양한 경험을 쌓아보세요.
                     </BenefitCard.Description.Kor>
                 </BenefitCard.Description>
             </BenefitModal.ModalContent>
