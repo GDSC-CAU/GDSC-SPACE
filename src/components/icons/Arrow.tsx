a7898c11d91ca57697f337eaa163edd8e114d63c
Danpa_cho <danpa725@cau.ac.kr>
2023-08-29 11:48:35 +0900

    fix: arrow stroke color issue

--- src/components/icons/Arrow.tsx
+++ src/components/icons/Arrow.tsx
@@ -3,7 +3,12 @@ import { SVGProps } from 'react'
 const SvgArrow = (props: SVGProps<SVGSVGElement>) => (
     <svg width={16} height={16} fill="none" xmlns="http://www.w3.org/2000/svg" strokeWidth={0} {...props}>
         <circle cx={8} cy={8} r={8} transform="rotate(90 8 8)" fill="tranparent" />
-        <path d="m7 4 3.646 3.646a.5.5 0 0 1 0 .708L7 12" stroke="white" strokeWidth={1.5} strokeLinecap="round" />
+        <path
+            d="m7 4 3.646 3.646a.5.5 0 0 1 0 .708L7 12"
+            stroke={props.stroke}
+            strokeWidth={1.5}
+            strokeLinecap="round"
+        />
     </svg>
 )
 export default SvgArrow
