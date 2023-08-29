d0fac069653321b2410323ecd4dc967b9944dfd5
Danpa_cho <danpa725@cau.ac.kr>
2023-08-29 16:08:04 +0900

    feat: add project mobile card component and rename desktop card

---
+++ src/app/_components/sections/projects/ProjectMobileCard.tsx
@@ -0,0 +1,49 @@
+'use client'
+import { MAIN_PROJECT_DATA } from 'src/interfaces/common'
+import { FadeIn, FillImage, IconButton, Link$ } from '~/components/common'
+import { Arrow } from '~/src/components/icons'
+
+interface ProjectCardMobileProps {
+    projectData: MAIN_PROJECT_DATA
+    fromLeft: boolean
+}
+export const ProjectCardMobile = ({ projectData, fromLeft }: ProjectCardMobileProps) => {
+    const { PROJECT_ID, PROJECT_IMAGE, PROJECT_IMAGE_SUB, PROJECT_SUBTITLE, PROJECT_TITLE } = projectData
+
+    return (
+        <FadeIn
+            twClass="w-full flex gap-4 flex-col items-center justify-center lg:hidden"
+            from={`${fromLeft ? '-translate-x-full' : 'translate-x-full'}`}
+            to="translate-x-0"
+            duration="1000"
+            observerOption={{
+                rootMargin: '0px 100px 100px 0px',
+            }}
+        >
+            <div className="mb-4 flex flex-col items-center justify-center gap-0">
+                <h1 className="font-eng text-4xl font-semibold text-white">{PROJECT_TITLE}</h1>
+                <h2 className="font-eng text-xl font-light text-white">{PROJECT_SUBTITLE}</h2>
+            </div>
+
+            <Link$ href={`/blog/${PROJECT_ID}`} className="h-[400px] w-full">
+                <FillImage
+                    src={PROJECT_IMAGE}
+                    alt={PROJECT_TITLE}
+                    containerClass="w-full h-full overflow-hidden rounded-2xl transition-all duration-1000 ease-in-out"
+                    imageClass="rounded-2xl"
+                >
+                    <IconButton twClass="z-10 absolute bottom-4 right-4">
+                        <Arrow />
+                    </IconButton>
+                </FillImage>
+            </Link$>
+
+            <FillImage
+                src={PROJECT_IMAGE_SUB}
+                alt={PROJECT_SUBTITLE}
+                containerClass="w-full h-[175px] rounded-2xl"
+                imageClass="rounded-2xl"
+            />
+        </FadeIn>
+    )
+}
