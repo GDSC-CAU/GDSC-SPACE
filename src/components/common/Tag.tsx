f9d329dd7d5eb89322d903b13beb89bcf0c49466
Danpa_cho <danpa725@cau.ac.kr>
2023-08-29 11:48:56 +0900

    refactor: add onClick props at Tag common component

--- src/components/common/Tag.tsx
+++ src/components/common/Tag.tsx
@@ -2,16 +2,18 @@ import { TailwindComponent } from './tailwind'
 
 interface TagProps extends TailwindComponent {
     isActive: boolean
+    onClick?: () => void
 }
 
-export const Tag = ({ isActive, children, twClass }: React.PropsWithChildren<TagProps>) => {
+export const Tag = ({ isActive, children, onClick, twClass }: React.PropsWithChildren<TagProps>) => {
     return (
         <button
             type="button"
+            onClick={onClick}
             className={`${twClass} flex items-center justify-center rounded-full border px-4 py-1 font-eng text-base opacity-100 transition-colors duration-300 hover:border-primary-blue active:opacity-75 md:px-7 md:py-2 md:text-2xl ${
                 isActive
                     ? 'border-primary-blue bg-primary-blue font-medium text-white'
-                    : 'border-primary-whitegray bg-transparent font-normal'
+                    : 'border-primary-whitegray bg-transparent font-light'
             }`}
         >
             {children}
