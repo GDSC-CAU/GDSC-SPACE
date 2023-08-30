7360e6c647fdbd719dd7b2e43fa43e9fef224b31
jujured <pjiww4@gmail.com>
2023-08-30 21:55:41 +0900

    feat:멤버페이지 그라데이션 구현 완료~!

--- src/app/members/_components/MemberHorizontalScrollView.tsx
+++ src/app/members/_components/MemberHorizontalScrollView.tsx
@@ -12,10 +12,10 @@ function divideArrayIntoChunk<T>(arr: T[], chunkSize: number) {
 
 const GradientBackground = ({ rowIndex }: { rowIndex: number }) => (
     <div
-        className={`z-10 px-52${
+        className={`absolute z-10 h-[20rem] w-[5rem] ${
             rowIndex % 2 === 0
-                ? 'absolute right-0 h-[18rem] w-[4rem] bg-gradient-to-l from-primary-hotpink to-transparent'
-                : 'absolute left-0 h-[18rem] w-[4rem] bg-gradient-to-r from-primary-blue to-transparent'
+                ? 'right-0 bg-gradient-to-l from-primary-darkgray to-transparent'
+                : 'left-0 bg-gradient-to-r from-primary-darkgray to-transparent'
         }`}
     />
 )
@@ -29,6 +29,7 @@ export const MemberHorizontalScrollView = ({ members }: RenderMemberByYearProps)
 
     return dividedMembers.map((memberChunk, rowIndex) => (
         <div key={rowIndex} className="relative">
+            <GradientBackground rowIndex={rowIndex} />
             <div
                 className={`flex flex-row gap-8 overflow-x-auto overflow-y-hidden px-4 py-7 scrollbar-hide ${
                     rowIndex % 2 === 0 ? 'flex-row' : 'flex-row-reverse'
@@ -38,7 +39,6 @@ export const MemberHorizontalScrollView = ({ members }: RenderMemberByYearProps)
                     <MemberCard key={`${member.MEMBER_NAME}-${member.MEMBER_POSITION}`} member={member} />
                 ))}
             </div>
-            <GradientBackground rowIndex={rowIndex} />
         </div>
     ))
 }
