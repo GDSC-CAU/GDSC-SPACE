6a83ec5a9657890527c4d0da80ab0e417eeea36f
Danpa_cho <danpa725@cau.ac.kr>
2023-08-30 23:52:19 +0900

    feat: fetch member data and render

--- src/app/members/[year]/page.tsx
+++ src/app/members/[year]/page.tsx
@@ -1,4 +1,28 @@
-//TODO: year params based server component
-export default async function Members() {
-    return <></>
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import type { API_MEMBER_LIST } from '~/interfaces/Common'
+import { MEMBER_PAGE_PARAMS } from '~/src/interfaces'
+import { Fetcher } from '~/src/utils'
+import { MemberHorizontalScrollView } from '../_components'
+
+const fetcher = new Fetcher({
+    baseUrl: 'http://localhost:3000',
+})
+
+export default async function MemberPage({ params: { year } }: MEMBER_PAGE_PARAMS) {
+    const members = await fetcher.get<API_MEMBER_LIST>(`/members/getList/${year}`)
+    const isMemberExist = members.MEMBER_LIST?.length !== 0
+
+    return (
+        <>
+            {isMemberExist ? (
+                <div className="flex flex-col py-4">
+                    <MemberHorizontalScrollView members={members.MEMBER_LIST!} />
+                </div>
+            ) : (
+                <div className="flex flex-col py-4">
+                    <p className="font-serif text-3xl">Member {year} is not exists</p>
+                </div>
+            )}
+        </>
+    )
 }
