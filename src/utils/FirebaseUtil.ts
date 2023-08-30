b50bc25a2c82885570603d7499160d658f4c67fd
yymin1022 <yymin1022@gmail.com>
2023-08-31 03:14:33 +0900

    feat: Added Initialization of Firebase

--- src/utils/FirebaseUtil.ts
+++ src/utils/FirebaseUtil.ts
@@ -2,10 +2,10 @@ import { FirebaseApp, initializeApp } from '@firebase/app'
 import { Firestore, getFirestore } from '@firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: process.env.FB_API_KEY,
-    authDomain: process.env.FB_AUTH_DOMAIN,
-    projectId: process.env.FB_PROJECT_ID,
-    appId: process.env.FB_APP_ID,
+    apiKey: process.env.NEXT_PUBLIC_FB_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FB_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FB_PROJECT_ID,
+    appId: process.env.NEXT_PUBLIC_FB_APP_ID,
 }
 
 let firebaseApp: FirebaseApp
