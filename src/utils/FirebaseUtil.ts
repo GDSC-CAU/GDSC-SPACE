ddc15e693de2436b02238728607803435f4a6ad9
yymin1022 <yymin1022@gmail.com>
2023-08-31 03:22:14 +0900

    feat: Added Main API Functions to Firebase Util

--- src/utils/FirebaseUtil.ts
+++ src/utils/FirebaseUtil.ts
@@ -1,5 +1,6 @@
 import { FirebaseApp, initializeApp } from '@firebase/app'
 import { Firestore, getFirestore } from '@firebase/firestore'
+import { API_MAIN_PROJECTS, API_MAIN_TIMELINES } from '~/src/interfaces'
 
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FB_API_KEY,
@@ -14,4 +15,30 @@ let firebaseDB: Firestore
 const initFirebase = () => {
     firebaseApp = initializeApp(firebaseConfig)
     firebaseDB = getFirestore()
+}
+
+export const getMainProjectDB = async () => {
+    if (firebaseApp === undefined || firebaseDB === undefined) {
+        initFirebase()
+    }
+
+    const ProjectList: API_MAIN_PROJECTS = {
+        MAIN_PROJECT_CNT: 0,
+        MAIN_PROJECT_LIST: [],
+    }
+
+    return ProjectList
+}
+
+export const getMainTimelineDB = async () => {
+    if (firebaseApp === undefined || firebaseDB === undefined) {
+        initFirebase()
+    }
+
+    const TimelineList: API_MAIN_TIMELINES = {
+        MAIN_TIMELINE_CNT: 0,
+        MAIN_TIMELINE_LIST: [],
+    }
+
+    return TimelineList
 }
