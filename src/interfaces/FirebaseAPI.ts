7c9c295232f77ed7ba21d48c95f6dd73efcefbc5
yymin1022 <yymin1022@gmail.com>
2023-08-31 03:38:28 +0900

    feat: Added Interface for Firebase API

--- src/interfaces/FirebaseAPI.ts
+++ src/interfaces/FirebaseAPI.ts
@@ -1,3 +1,11 @@
+export interface MainProjects {
+    Description: string
+    Image: string
+    Image_sub: string
+    Subtitle: string
+    Title: string
+}
+
 export interface MainTimeline {
     Card_title: string
     Date: string
