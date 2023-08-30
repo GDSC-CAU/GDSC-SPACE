5dc9d7ba05b8a5b51952c67e9f6dd62ecbab93aa
yymin1022 <yymin1022@gmail.com>
2023-08-31 03:52:12 +0900

    feat: Added Interface for Firebase API

--- src/interfaces/FirebaseAPI.ts
+++ src/interfaces/FirebaseAPI.ts
@@ -13,3 +13,17 @@ export interface MainTimelines {
     Description: string
     Title: string
 }
+
+export interface Members {
+    Behance: string
+    Comment: string
+    Email: string
+    Gender: string
+    Github: string
+    Image: string
+    Instagram: string
+    Name: string
+    Nickname: string
+    Position: string
+    Role: string
+}
