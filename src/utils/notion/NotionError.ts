ea64262e2b230cb9c0b0616df7cc339cafcce593
Danpa_cho <danpa725@cau.ac.kr>
2023-08-31 22:00:27 +0900

    feat: add notion error class to handle api error

---
+++ src/utils/notion/NotionError.ts
@@ -0,0 +1,47 @@
+interface NotionErrorOption {
+    label?: string
+    queryId: string
+    error: unknown
+    type: 'DB' | 'BLOCK'
+}
+class NotionError extends Error {
+    public label?: string
+
+    public constructor(message: string, { label, queryId, error, type }: NotionErrorOption) {
+        super(message, {
+            cause: [
+                {
+                    code: 'NOTION_QUERY_ID',
+                    value: `${type} ${queryId}`,
+                },
+                {
+                    code: 'NOTION_API_ERROR',
+                    value: typeof error === 'string' ? error : error instanceof Error ? error : JSON.stringify(error),
+                },
+            ],
+        })
+        this.label = label
+    }
+}
+
+type NotionDbErrorOption = Omit<NotionErrorOption, 'type' | 'label'>
+export class NotionDbError extends NotionError {
+    public constructor(message: string, option: NotionDbErrorOption) {
+        super(message, {
+            ...option,
+            type: 'DB',
+            label: 'Notion DB Error Occurred',
+        })
+    }
+}
+
+type NotionBlockErrorOption = Omit<NotionErrorOption, 'type' | 'label'>
+export class NotionBlockError extends NotionError {
+    public constructor(message: string, option: NotionBlockErrorOption) {
+        super(message, {
+            ...option,
+            type: 'BLOCK',
+            label: 'Notion Block parsing Error Occurred',
+        })
+    }
+}
