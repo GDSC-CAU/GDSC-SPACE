a598ef99264692927415b5d338b7cba7c4408c42
Danpa_cho <danpa725@cau.ac.kr>
2023-08-31 22:04:00 +0900

    feat: render notion page

--- src/app/blog/[type]/[id]/page.tsx
+++ src/app/blog/[type]/[id]/page.tsx
@@ -1,8 +1,25 @@
-export default function BlogView({ params }: { params: { id: string } }) {
-    const blogID = params.id
+import { NotionAPI } from 'notion-client'
+import { Divider } from '~/src/components/common'
+import { BLOG_POST_PARAMS } from '~/src/interfaces'
+import { Notion } from './_components/NotionRenderer'
+
+const notion = new NotionAPI()
+
+export default async function BlogView({ params: { id } }: BLOG_POST_PARAMS) {
+    let recordMap: Awaited<ReturnType<typeof notion.getPage>> | null = null
+    try {
+        const res = await notion.getPage(id)
+        recordMap = res
+    } catch (e) {
+        // eslint-disable-next-line no-console
+        console.log(e)
+        recordMap = null
+    }
+
     return (
-        <main className="flex h-full w-full flex-col items-center justify-between p-24">
-            <p>This is Blog View of {blogID}</p>
+        <main className="h-full w-full">
+            <Divider />
+            {recordMap ? <Notion recordMap={recordMap} /> : <>404</>}
         </main>
     )
 }
