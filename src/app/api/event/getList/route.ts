c489693f3aaca24b9a0b1560650518ae04750eba
yymin1022 <yymin1022@gmail.com>
2023-09-01 00:41:34 +0900

    fix: Fixed Import Issue of Event List API

--- src/app/api/event/getList/route.ts
+++ src/app/api/event/getList/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { API_EVENT_LIST, API_RESPONSE } from '~/src/interfaces/Common'
+import { getEventListDB } from '~/utils/FirebaseUtil'
 
 export async function GET(_: NextRequest) {
     const apiResult: API_RESPONSE<API_EVENT_LIST> = {
