2ff346badfcbff7e3b940d684736d4593c3b1884
sonyujin <jainayj804998@gmail.com>
2023-09-01 15:41:06 +0900

    feat: event mobile error

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,9 +1,6 @@
 import { Divider } from '~/components/common'
 import { EVENT_META } from '~/interfaces/Common'
 import { EventCard } from './_components'
-import { EventCardDesktop } from './_components/EventCardDesktop'
-
-import { EventCardMobile } from './_components/EventCardMobile'
 
 const generateRandomEvent = (count: number): Array<EVENT_META> =>
     Array.from(
@@ -19,7 +16,6 @@ const generateRandomEvent = (count: number): Array<EVENT_META> =>
             EVENT_TYPE: 'Association',
             EVENT_THUMBNAIL: '/events/cardtest.png',
             EVENT_NOTION_ID: '',
-
         })
     )
 
@@ -28,21 +24,17 @@ const bulkData = generateRandomEvent(50)
 export default function EventsPage() {
     return (
         <>
-            <div className="flex h-full w-full flex-col items-center gap-1.5">
+            <div className="flex h-full w-full min-w-[400px] flex-col items-center gap-1.5">
                 <h1 className="mt-20 font-eng text-5xl font-bold">Event</h1>
                 <span className="text-xs">GDSC CAU가 진행한 행사들과 다가올 이벤트들을 확인하세요!</span>
             </div>
 
-            <div className="flex w-fit flex-col items-center justify-center pt-4">
+            <div className="flex w-full max-w-[800px] flex-col items-center justify-center pt-4">
                 <Divider twClass="w-full" />
 
-                <div className="grid w-full items-center gap-y-8 md: lg:grid-cols-3 md:mx-0.5">
+                <div className="flex max-w-[800px] flex-wrap items-center justify-center gap-x-4 gap-y-8">
                     {bulkData.map((event) => (
-                        <EventCardDesktop event={event} key={event.EVENT_ID} />
-                    ))}
-
-                    {bulkData.map((event, i) => (
-                        <EventCardMobile event={event} key={event.EVENT_ID} />
+                        <EventCard event={event} key={event.EVENT_ID} />
                     ))}
                 </div>
             </div>
