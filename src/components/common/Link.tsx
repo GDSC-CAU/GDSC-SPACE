3f6621aa910b7cbf92148614cbd4b7be2850a98f
Danpa_cho <danpa725@cau.ac.kr>
2023-09-02 15:58:08 +0900

    refactor: add Url type at Link$ common component

--- src/components/common/Link.tsx
+++ src/components/common/Link.tsx
@@ -1,8 +1,10 @@
+import { UrlObject } from 'url'
 import Link, { type LinkProps } from 'next/link'
 import { type BlogRoutingType } from '~/src/app/blog/_data/blogRouteData'
 
 type BlogRoute = `/blog/${BlogRoutingType}`
 type UNIQUE_ID = string
+
 /**
  * Project link path
  */
@@ -14,8 +16,12 @@ export type LinkPath =
     | '/events'
     | `/events/${UNIQUE_ID}`
 
+type ExtendedUrl = UrlObject & {
+    pathname: LinkPath
+}
+
 interface Link$Props extends Omit<React.AnchorHTMLAttributes<HTMLAnchorElement>, keyof LinkProps>, LinkProps {
-    href: LinkPath
+    href: LinkPath | ExtendedUrl
 }
 export const Link$ = ({ href, children, ...linkProps }: React.PropsWithChildren<Link$Props>) => (
     <Link {...linkProps} href={href}>
