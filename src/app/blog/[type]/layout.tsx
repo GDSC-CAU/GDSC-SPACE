7edd7b318c99d90d7c3c985520a764b8ed4cb917
Danpa_cho <danpa725@cau.ac.kr>
2023-09-02 16:02:41 +0900

    refactor: extract BlogHeader component at layout.tsx

--- src/app/blog/[type]/layout.tsx
+++ src/app/blog/[type]/layout.tsx
@@ -1,10 +1,11 @@
 import { Footer } from '~/src/components/common'
 import type { BLOG_TYPE_PARAMS } from '~/src/interfaces'
-// import { BlogHeader } from '../_components/BlogHeader'
+import { BlogHeader } from '../_components/BlogHeader'
 
 export default function BlogLayout({ params: { type }, children }: React.PropsWithChildren<BLOG_TYPE_PARAMS>) {
     return (
         <main className="flex h-full w-full min-w-[50%] flex-col items-center justify-between gap-4 md:gap-7">
+            <BlogHeader type={''} />
             {children}
             <Footer />
         </main>
