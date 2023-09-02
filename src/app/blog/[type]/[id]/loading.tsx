9658e116fa608bf66dae3c70d61756ab597095bb
Danpa_cho <danpa725@cau.ac.kr>
2023-09-02 16:02:17 +0900

    feat: add skeleton loading ui at post page

--- src/app/blog/[type]/[id]/loading.tsx
+++ src/app/blog/[type]/[id]/loading.tsx
@@ -1,17 +1,18 @@
 export default function BlogPostLoading() {
-    //TODO: 여기는 다르게 스켈레톤 UI 보여줘야 함
     return (
-        <section className="flex h-full w-full min-w-[50%] animate-pulse flex-col items-center justify-center gap-12">
-            <div className="w-full flex-col items-center justify-center gap-10 pt-10">
-                {[...Array(6)].map((_, index) => (
-                    <div
-                        className="my-10 flex h-fit w-full animate-pulse flex-col gap-4 rounded-xl bg-black p-4"
-                        key={index}
-                    >
-                        <div className="h-4 w-32 rounded-xl bg-primary-darkgray"></div>
-                        <div className="h-20 w-full rounded-xl bg-primary-darkgray"></div>
-                    </div>
-                ))}
+        <section className="flex h-screen w-full min-w-[50%] animate-pulse flex-col items-center justify-center gap-12">
+            <div className="my-10 flex h-full w-full animate-pulse flex-col gap-4 rounded-xl bg-black p-4">
+                <div className="h-4 w-32 rounded-xl bg-primary-darkgray"></div>
+                <div className="h-20 w-full rounded-xl bg-primary-darkgray"></div>
+                <div className="flex h-full w-full flex-col items-start justify-center gap-8 rounded-xl bg-primary-darkgray">
+                    <div className="h-4 w-full bg-black"></div>
+                    <div className="h-4 w-full bg-black"></div>
+                    <div className="h-4 w-full bg-black"></div>
+                    <div className="h-4 w-full bg-black"></div>
+                    <div className="h-4 w-full bg-black"></div>
+                    <div className="h-4 w-full bg-black"></div>
+                    <div className="h-4 w-full bg-black"></div>
+                </div>
             </div>
         </section>
     )
