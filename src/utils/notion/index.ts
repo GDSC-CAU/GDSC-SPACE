172d5c0417a2d63bd36d205f95daf720e150e453
Danpa_cho <danpa725@cau.ac.kr>
2023-09-02 16:00:31 +0900

    feat: inject notion client at blogApi instance

--- src/utils/notion/index.ts
+++ src/utils/notion/index.ts
@@ -1,7 +1,9 @@
 import { Client } from '@notionhq/client'
+import { NotionAPI } from 'notion-client'
 import { NotionBlockManager } from './BlockManager'
 import { NotionBlogPost } from './BlogPost'
 import { NotionDbManager } from './DbManager'
+import { NotionPageManager } from './PageManager'
 
 const notionClient = new Client({
     auth: process.env.NOTION_API_KEY,
@@ -11,7 +13,11 @@ export const blogApi = new NotionBlogPost({
     notionDbManager: new NotionDbManager({
         notionClient,
     }),
-    notionPageManager: new NotionBlockManager({
+    notionBlockManager: new NotionBlockManager({
         notionClient,
     }),
+    notionPageManager: new NotionPageManager({
+        notionClient,
+    }),
+    notionXClient: new NotionAPI(),
 })
