9bec4d2c375c3cdfc03f4ebb517987c78fe75b8d
pjw-redt <pjiww4@gmail.com>
2023-09-07 11:45:31 +0900

    feat:year Button 재정렬

--- src/app/members/[year]/layout.tsx
+++ src/app/members/[year]/layout.tsx
@@ -41,7 +41,7 @@ const yearList = [
 
 export default function MembersLayout({ children, params: { year } }: React.PropsWithChildren<MEMBER_PAGE_PARAMS>) {
     return (
-        <main className="flex h-full w-full flex-col items-center justify-between md:gap-10">
+        <main className="flex h-full w-full flex-col items-start justify-between md:gap-10">
             <div className="flex flex-row items-center justify-between gap-3 pl-1.5 md:gap-7 md:pb-5 md:pt-10">
                 {yearList.map(({ year: yearNumber, title }) => (
                     <YearButton key={yearNumber} year={yearNumber} isYearActive={yearNumber === year}>
