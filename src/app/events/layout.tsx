1821ba3baceb0b3b85b066a860541c0fa866422e
Danpa_cho <danpa725@cau.ac.kr>
2023-09-16 13:31:02 +0900

    design: disable clicks at event background svg

--- src/app/events/layout.tsx
+++ src/app/events/layout.tsx
@@ -3,7 +3,7 @@ import { EventRound } from '~/src/components/icons'
 export default function EventLayout({ children }: React.PropsWithChildren) {
     return (
         <main className="relative flex min-w-[50%] flex-col items-center justify-center">
-            <EventRound className="absolute -top-96 left-1/2 -translate-x-1/2 scale-[0.575]" />
+            <EventRound className="pointer-events-none absolute -top-96 left-1/2 -z-50 -translate-x-1/2 scale-[0.575]" />
             {children}
         </main>
     )
