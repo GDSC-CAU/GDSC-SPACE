ec2d7213be479af940f32256031de4ff41c95474
Danpa_cho <danpa725@cau.ac.kr>
2023-09-16 13:20:45 +0900

    fix: projects section x overflow issue with `overflow-x-hidden`

--- src/app/_components/sections/projects/ProjectsSection.tsx
+++ src/app/_components/sections/projects/ProjectsSection.tsx
@@ -5,7 +5,7 @@ import { ProjectCardMobile } from './ProjectMobileCard'
 
 export const ProjectsSection = ({ MAIN_PROJECT_LIST }: API_MAIN_PROJECTS) => {
     return (
-        <section className="mt-28 flex flex-col items-center justify-center gap-6 pb-20">
+        <section className="mt-28 flex h-full w-full flex-col items-center justify-center gap-6 overflow-x-hidden pb-20">
             <GradientHeader id="projects">Projects</GradientHeader>
             <div className="mb-12">우리는 세상의 문제를 찾고 새로운 해결책을 탐구합니다.</div>
 
@@ -21,5 +21,3 @@ export const ProjectsSection = ({ MAIN_PROJECT_LIST }: API_MAIN_PROJECTS) => {
         </section>
     )
 }
-
-//flex flex-col gap-7 md:grid md:grid-flow-col md:grid-cols-7 md:grid-rows-6 md:gap-5
