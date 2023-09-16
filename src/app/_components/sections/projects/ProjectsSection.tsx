c45c16ca48f8fcf90aa14f99560ccdd247d2ff82
Danpa_cho <danpa725@cau.ac.kr>
2023-09-16 13:24:53 +0900

    fix: clipped x-side desktop project card

--- src/app/_components/sections/projects/ProjectsSection.tsx
+++ src/app/_components/sections/projects/ProjectsSection.tsx
@@ -9,9 +9,9 @@ export const ProjectsSection = ({ MAIN_PROJECT_LIST }: API_MAIN_PROJECTS) => {
             <GradientHeader id="projects">Projects</GradientHeader>
             <div className="mb-12">우리는 세상의 문제를 찾고 새로운 해결책을 탐구합니다.</div>
 
-            <div className="flex w-full flex-col items-center justify-center gap-4 gap-y-20 md:grid md:grid-flow-col md:grid-rows-2 md:gap-4">
+            <div className="flex h-full w-full flex-col items-center justify-between gap-20 md:max-w-4xl">
                 {MAIN_PROJECT_LIST.map((project, i) => (
-                    <ProjectCardDesktop key={project.PROJECT_ID} isReverse={i % 2 === 0} projectData={project} />
+                    <ProjectCardDesktop key={project.PROJECT_ID} isMainBannerLeft={i % 2 === 0} projectData={project} />
                 ))}
 
                 {MAIN_PROJECT_LIST.map((project, i) => (
