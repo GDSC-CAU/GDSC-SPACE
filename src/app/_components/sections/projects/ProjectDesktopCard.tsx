602e8e3ca4c7ab5f2c8bdb2a3c552f972272c062
Danpa_cho <danpa725@cau.ac.kr>
2023-09-16 13:24:27 +0900

    refactor: project card desktop component clipped x-side issue

--- src/app/_components/sections/projects/ProjectDesktopCard.tsx
+++ src/app/_components/sections/projects/ProjectDesktopCard.tsx
@@ -1,67 +1,83 @@
 'use client'
-import { FillImage, IconButton, Link$, useVisibility } from '~/components/common'
+import { FadeIn, FillImage, IconButton, Link$ } from '~/components/common'
 import { Arrow } from '~/src/components/icons'
 import type { MAIN_PROJECT_DATA } from '~/src/interfaces/Common'
 
 interface ProjectCardDesktopProps {
-    isReverse: boolean
+    isMainBannerLeft: boolean
     projectData: MAIN_PROJECT_DATA
 }
-export const ProjectCardDesktop = ({ isReverse, projectData }: ProjectCardDesktopProps) => {
-    const textAlign = isReverse ? 'text-end' : 'text-start'
-    const LCardMargin = isReverse ? 'ml-[20px]' : 'mr-[-20px]'
-    const RCardMargin = isReverse ? 'ml-[20px]' : 'mr-[-20px]'
-    const LScrollFrom = isReverse
-        ? 'translate-y-16 -translate-x-full opacity-0 scale-75'
-        : 'translate-y-16 translate-x-[200%] opacity-0 scale-75'
-    const LScrollTo = isReverse ? '-translate-x-0 opacity-100 scale-100' : 'translate-x-full opacity-100 scale-100'
-    const RScrollFrom = isReverse
-        ? 'translate-y-16 translate-x-full opacity-0 scale-75'
-        : 'translate-y-16 -translate-x-[200%] opacity-0 scale-75'
-    const RScrollTo = isReverse ? 'translate-x-0 opacity-100 scale-100' : '-translate-x-full opacity-100 scale-100'
+export const ProjectCardDesktop = ({ isMainBannerLeft, projectData }: ProjectCardDesktopProps) => {
+    const {
+        PROJECT_ID: id,
+        PROJECT_TITLE: title,
+        PROJECT_SUBTITLE: subTitle,
+        PROJECT_DESCRIPTION: description,
+        PROJECT_IMAGE: imageSrc,
+        PROJECT_IMAGE_SUB: subImageSrc,
+    } = projectData
 
-    const { isVisible, setVisibilityRef } = useVisibility<HTMLDivElement>()
+    const fromLeft = '-translate-y-16 -translate-x-1/2 opacity-0 scale-75'
+    const fromRight = '-translate-y-16 translate-x-1/2 opacity-0 scale-75'
+    const toCenter = 'translate-0 opacity-100 scale-100'
+
+    const mainBannerHeight = 'h-[500px]'
+    const subBannerHeight = 'h-full max-h-[200px]'
+
+    const textAlign = isMainBannerLeft ? 'text-end' : 'text-start'
 
     return (
-        <div className="group hidden flex-row items-center justify-center md:flex" ref={setVisibilityRef}>
-            <Link$ href={`/blog/Project/${projectData.PROJECT_ID}`}>
+        <Link$
+            href={`/blog/Project/${id}`}
+            className={`${
+                isMainBannerLeft ? 'flex-row' : 'flex-row-reverse'
+            } hidden h-full w-full items-start justify-center gap-4 md:flex`}
+        >
+            <FadeIn
+                animationTiming="ease"
+                duration="1000"
+                from={isMainBannerLeft ? fromLeft : fromRight}
+                to={toCenter}
+                observerOption={{
+                    rootMargin: '100%',
+                }}
+                twClass="w-1/2 h-full group"
+            >
                 <FillImage
-                    src={projectData.PROJECT_IMAGE}
-                    alt={projectData.PROJECT_TITLE}
-                    containerClass={`transform-gpu h-[480px] w-[420px] overflow-hidden rounded-xl transition-all duration-1000 ease-in-out relative ${LCardMargin} ${
-                        isVisible ? LScrollTo : LScrollFrom
-                    }`}
-                    imageClass="rounded-xl group-hover:scale-110 group-hover:brightness-110 transition-all duration-500"
+                    alt={`${title} project image`}
+                    src={imageSrc}
+                    containerClass={`w-full ${mainBannerHeight} relative`}
+                    imageClass="w-full h-full rounded-xl"
                 >
                     <IconButton twClass="absolute opacity-0 bottom-4 right-4 group-hover:opacity-100 transition-opacity duration-300">
                         <Arrow className="stroke-black" />
                     </IconButton>
                 </FillImage>
-            </Link$>
-            <div
-                className={`flex h-[480px] w-[420px] flex-col transition-all duration-1000 ease-in-out transform-gpu ${RCardMargin} ${
-                    isVisible ? RScrollTo : RScrollFrom
-                }`}
+            </FadeIn>
+
+            <FadeIn
+                animationTiming="ease"
+                duration="1000"
+                from={isMainBannerLeft ? fromRight : fromLeft}
+                to={toCenter}
+                observerOption={{
+                    rootMargin: '100%',
+                }}
+                twClass={`w-1/2 ${mainBannerHeight} flex flex-col items-center gap-10`}
             >
-                <section className={`flex h-full w-full flex-col items-end justify-start pb-10`}>
-                    <h1 className={`w-full font-eng text-6xl font-bold text-white ${textAlign}`}>
-                        {projectData.PROJECT_TITLE}
-                    </h1>
-                    <h2 className={`mb-10 w-full font-eng text-3xl font-normal text-white ${textAlign}`}>
-                        {projectData.PROJECT_SUBTITLE}
-                    </h2>
-                    <p className={`w-full font-eng text-sm font-light ${textAlign}`}>
-                        {projectData.PROJECT_DESCRIPTION}
-                    </p>
+                <section className={`flex h-full w-full flex-col items-end justify-start font-eng text-white`}>
+                    <h1 className={`w-full text-6xl font-bold ${textAlign}`}>{title}</h1>
+                    <h2 className={`mb-10 w-full text-3xl font-normal ${textAlign}`}>{subTitle}</h2>
+                    <p className={`w-full text-base font-light text-primary-whitegray ${textAlign}`}>{description}</p>
                 </section>
 
                 <FillImage
-                    src={projectData.PROJECT_IMAGE_SUB}
-                    alt={projectData.PROJECT_SUBTITLE}
-                    containerClass="h-[280px] w-[420px]"
-                    imageClass="rounded-xl"
+                    src={subImageSrc}
+                    alt={`${subTitle} project sub image`}
+                    containerClass={`w-full ${subBannerHeight}`}
+                    imageClass="w-full h-full rounded-xl"
                 />
-            </div>
-        </div>
+            </FadeIn>
+        </Link$>
     )
 }
