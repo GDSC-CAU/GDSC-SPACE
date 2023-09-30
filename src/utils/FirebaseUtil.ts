73268ce462630ce8f8513004c2004c2c447746fc
yymin1022 <yymin1022@gmail.com>
2023-09-30 22:20:57 +0900

    fix: Hide Firebase API Keys from ENV Values

--- src/utils/FirebaseUtil.ts
+++ src/utils/FirebaseUtil.ts
@@ -11,10 +11,10 @@ import {
 } from '~/src/interfaces'
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FB_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FB_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FB_PROJECT_ID,
-    appId: process.env.NEXT_PUBLIC_FB_APP_ID,
+    apiKey: process.env.FB_API_KEY,
+    authDomain: process.env.FB_AUTH_DOMAIN,
+    projectId: process.env.FB_PROJECT_ID,
+    appId: process.env.FB_APP_ID,
 }
 
 let firebaseApp: FirebaseApp
