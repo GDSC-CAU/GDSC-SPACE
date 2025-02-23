ec049c59cf87eadef37832a3082cbb32958b6920
chae1xx1os <kty1895@naver.com>
2025-02-23 14:57:44 +0900

    feat: add Organizer, Team Member role (#146)

--- src/app/members/_components/MemberRoleIcon.tsx
+++ src/app/members/_components/MemberRoleIcon.tsx
@@ -1,13 +1,22 @@
-import { Core, Lead } from '~/components/icons'
+import { Core, Lead, Organizer, TeamMember } from '~/components/icons'
 import { MEMBER_DATA } from '~/src/interfaces/Common'
 
-export const MemberRoleIcon = ({ MEMBER_ROLE }: Pick<MEMBER_DATA, 'MEMBER_ROLE'>) => (
-    <>
-        {MEMBER_ROLE === 'Lead' && (
-            <Lead className="absolute right-[-0.5rem] top-[1.15rem] scale-125 md:right-[-0.6rem] md:top-7" />
-        )}
-        {MEMBER_ROLE === 'Core' && (
-            <Core className="absolute right-[-0.5rem] top-[1.15rem] scale-125 md:right-[-0.6rem] md:top-7" />
-        )}
-    </>
-)
+export const MemberRoleIcon = ({ MEMBER_ROLE }: Pick<MEMBER_DATA, 'MEMBER_ROLE'>) => {
+    console.log('current MEMBER_ROLE:', MEMBER_ROLE)
+    return (
+        <>
+            {MEMBER_ROLE === 'Lead' && (
+                <Lead className="absolute right-[-0.5rem] top-[1.15rem] scale-125 md:right-[-0.6rem] md:top-7" />
+            )}
+            {MEMBER_ROLE === 'Core' && (
+                <Core className="absolute right-[-0.5rem] top-[1.15rem] scale-125 md:right-[-0.6rem] md:top-7" />
+            )}
+            {MEMBER_ROLE === 'Organizer' && (
+                <Organizer className="absolute right-[-0.5rem] top-[1.15rem] scale-125 md:right-[-0.6rem] md:top-7" />
+            )}
+            {MEMBER_ROLE === 'Team Member' && (
+                <TeamMember className="absolute right-[-0.5rem] top-[1.15rem] scale-125 md:right-[-0.6rem] md:top-7" />
+            )}
+        </>
+    )
+}
