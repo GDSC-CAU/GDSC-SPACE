ddf4d86e10da325d10d4b0f7e98ab6aca01bb8e3
ye-one222 <101704967+ye-one222@users.noreply.github.com>
2025-02-23 15:24:14 +0900

    fix: adjust font size for mobile

--- src/app/members/_components/MemberContact.tsx
+++ src/app/members/_components/MemberContact.tsx
@@ -8,7 +8,7 @@ export const MemberContact = ({ contact, icon }: MemberContactProps) => {
     return (
         <div className="z-20 flex w-full flex-row gap-1">
             {icon}
-            <p className="w-[6rem] truncate text-left font-eng text-5xs text-black md:w-[9rem] md:text-xs">{contact}</p>
+            <p className="w-24 truncate text-left font-eng text-3xs text-black md:w-36 md:text-xs">{contact}</p>
         </div>
     )
 }
