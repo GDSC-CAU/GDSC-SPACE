49e6bf29ba0d5297362b6087c0942a8ea422f8c4
causyj <jainayj804998@gmail.com>
2025-02-27 08:59:21 +0900

    feat: add member tag

--- src/app/members/_components/MemberRoleIcon.tsx
+++ src/app/members/_components/MemberRoleIcon.tsx
@@ -1,6 +1,6 @@
 import { Core, Lead, Organizer, TeamMember } from '~/components/icons'
 import { MEMBER_DATA } from '~/src/interfaces/Common'
-import { SVGCore, SVGLead } from '~/src/styles/icons'
+import { SVGCore, SVGLead, SVGOrg, SVGTm } from '~/src/styles/icons'
 
 export const MemberRoleIcon = ({ MEMBER_ROLE }: Pick<MEMBER_DATA, 'MEMBER_ROLE'>) => (
     <>
@@ -8,13 +8,13 @@ export const MemberRoleIcon = ({ MEMBER_ROLE }: Pick<MEMBER_DATA, 'MEMBER_ROLE'>
             <SVGLead className="absolute right-[-0.5rem] top-[1.15rem] scale-125 md:right-[-0.6rem] md:top-7" />
         )}
         {MEMBER_ROLE === 'Organizer' && (
-            <SVGCore className="absolute right-[-0.5rem] top-[1.15rem] scale-125 md:right-[-0.6rem] md:top-7" />
+            <SVGOrg className="absolute right-[-0.5rem] top-[1.15rem] scale-125 md:right-[-0.6rem] md:top-7" />
         )}
         {MEMBER_ROLE === 'Core' && (
             <SVGCore className="absolute right-[-0.5rem] top-[1.15rem] scale-125 md:right-[-0.6rem] md:top-7" />
         )}
         {MEMBER_ROLE === 'Team Member' && (
-            <TeamMember className="absolute right-[-0.5rem] top-[1.15rem] scale-125 md:right-[-0.6rem] md:top-7" />
+            <SVGTm className="absolute right-[-0.5rem] top-[1.15rem] scale-125 md:right-[-0.6rem] md:top-7" />
         )}
     </>
 )
--- src/styles/icons/index.ts
+++ src/styles/icons/index.ts
@@ -1,2 +1,4 @@
 export { default as SVGCore } from './SVGCore.svg'
 export { default as SVGLead } from './SVGLead.svg'
+export { default as SVGOrg } from './SVGOrg.svg'
+export { default as SVGTm } from './SVGTm.svg'
