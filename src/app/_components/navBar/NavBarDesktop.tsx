1af250650fefe667295c11960ff06900721d7975
heejung <gimm9567@gmail.com>
2025-03-10 19:24:49 +0900

    feat: add link text

--- src/app/_components/navBar/NavBarDesktop.tsx
+++ src/app/_components/navBar/NavBarDesktop.tsx
@@ -9,7 +9,12 @@ export const NavBarDesktop = ({ iconSize = 55 }: NavBarProps) => {
     return (
         <nav className="sticky top-0 z-50 hidden h-20 flex-row items-center justify-center gap-2 bg-theme-background/5 py-4 backdrop-blur-lg md:flex md:gap-16">
             <NavButton type="link" href="/" disableUnderline>
-                <Gdsc width={iconSize} height={iconSize} className="scale-75 md:scale-100" />
+                <Gdsc
+                    width={iconSize}
+                    height={iconSize}
+                    className="scale-75 md:scale-100"
+                    aria-label="go to homepage"
+                />
             </NavButton>
 
             <NavButton type="scroll" href="/#about">
